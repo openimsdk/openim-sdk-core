ed0b49748ea23c96f972411e0dcd42fdda87f569
Gordon <1432970085@qq.com>
2021-12-10 12:04:58 +0800

    ws add set and get conversation recv opt

--- open_im_sdk/open_im_sdk_conversation_msg.go
+++ open_im_sdk/open_im_sdk_conversation_msg.go
@@ -870,7 +870,6 @@ func (u *UserRelated) SendMessage(callback SendMsgCallBack, message, receiver, g
 			conversationID = GetConversationIDBySessionType(receiver, SingleChatType)
 			c.UserID = receiver
 			c.ConversationType = SingleChatType
-
 			faceUrl, name, err := u.getUserNameAndFaceUrlByUid(receiver)
 			if err != nil {
 				sdkLog("getUserNameAndFaceUrlByUid err:", err)
@@ -1322,7 +1321,13 @@ func (u *UserRelated) MarkC2CMessageAsRead(callback Base, receiver string, msgID
 	go func() {
 		conversationID := GetConversationIDBySessionType(receiver, SingleChatType)
 		_ = u.triggerCmdUpdateConversation(updateConNode{ConId: conversationID, Action: UnreadCountSetZero})
-		if len(msgIDList) == 0 {
+		var list []string
+		err := json.Unmarshal([]byte(msgIDList), &list)
+		if err != nil {
+			callback.OnError(201, "json unmarshal err")
+			return
+		}
+		if len(list) == 0 {
 			callback.OnError(200, "msg list is null")
 			return
 		}
@@ -1330,7 +1335,7 @@ func (u *UserRelated) MarkC2CMessageAsRead(callback Base, receiver string, msgID
 		u.initBasicInfo(&s, UserMsgType, HasReadReceipt)
 		s.Content = msgIDList
 		sdkLog("MarkC2CMessageAsRead: send Message")
-		err := u.autoSendMsg(&s, receiver, "", false, false, false)
+		err = u.autoSendMsg(&s, receiver, "", false, false, false)
 		if err != nil {
 			sdkLog("MarkC2CMessageAsRead  err:", err.Error())
 			callback.OnError(300, err.Error())
@@ -1451,9 +1456,7 @@ func (u *UserRelated) ClearC2CHistoryMessage(callback Base, userID string) {
 			callback.OnSuccess("")
 			_ = u.triggerCmdUpdateConversation(updateConNode{ConId: conversationID, Action: ConAndUnreadChange})
 		}
-
 	}()
-
 }
 func (u *UserRelated) ClearGroupHistoryMessage(callback Base, groupID string) {
 	go func() {
@@ -1471,9 +1474,7 @@ func (u *UserRelated) ClearGroupHistoryMessage(callback Base, groupID string) {
 			callback.OnSuccess("")
 			_ = u.triggerCmdUpdateConversation(updateConNode{ConId: conversationID, Action: ConAndUnreadChange})
 		}
-
 	}()
-
 }
 
 func (u *UserRelated) InsertSingleMessageToLocalStorage(callback Base, message, userID, sender string) string {
