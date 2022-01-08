c453fb2d9a94c65d7a2264f74e63707296039d8f
wenxu12345 <44203734@qq.com>
2022-01-08 12:22:31 +0800

    Refactor code

--- internal/controller/interaction/ws.go
+++ internal/controller/interaction/ws.go
@@ -1,10 +1,10 @@
 package interaction
 
 import (
-	"github.com/golang/protobuf/proto"
-	"net/http"
+	"errors"
+	"github.com/gorilla/websocket"
+	"open_im_sdk/pkg/log"
 	"open_im_sdk/pkg/utils"
-	"os"
 	"time"
 )
 
@@ -13,86 +13,41 @@ type Ws struct {
 	WsConn
 }
 
-func (ws *Ws) WaitResp(ch chan GeneralWsResp, timeout int) (*GeneralWsResp, error) {
+func (ws *Ws) WaitResp(ch chan GeneralWsResp, timeout int, operationID string, connSend *websocket.Conn) (*GeneralWsResp, error) {
+	select {
+	case r := <-ch:
+		log.Info(operationID, "ws ch recvMsg success: ")
+		if r.ErrCode != 0 {
+			return nil, errors.New("errCode failed")
+		} else {
+			log.Info(operationID, "ws ch recvMsg success")
+			return &r, nil
+		}
 
+	case <-time.After(time.Second * time.Duration(timeout)):
+		log.Error(operationID, "ws ch recvMsg err, timeout")
+		if connSend != ws.conn {
+			return nil, errors.New("recv timeout, conn diff")
+		} else {
+			return nil, errors.New("recv timeout, conn same")
+		}
+	}
 }
 
 func (ws *Ws) SendReqWaitResp(buff []byte, reqIdentifier int32, timeout int, SenderID string) (*GeneralWsResp, error) {
 	var wsReq GeneralWsReq
 	wsReq.ReqIdentifier = reqIdentifier
 	wsReq.OperationID = utils.OperationIDGenerator()
+	ws.Lock()
 	msgIncr, ch := ws.AddCh(SenderID)
+	ws.Unlock()
 	wsReq.SendID = SenderID
-	//wsReq.Token = u.token
 	wsReq.MsgIncr = msgIncr
 	wsReq.Data = buff
-	err, _ := ws.writeBinaryMsg(wsReq)
+	err, connSend := ws.writeBinaryMsg(wsReq)
 	if err != nil {
+		log.Error(wsReq.OperationID, "ws send err ", err.Error(), wsReq)
 		return nil, err
 	}
-	select {
-	case r := <-ch:
-
-		if r.ErrCode != 0 {
-			return nil, err
-		} else {
-			callback.OnSuccess("")
-			callback.OnProgress(100)
-			for _, v := range delFile {
-				err := os.Remove(v)
-				if err != nil {
-					sdkLog("remove failed,", err.Error(), v)
-				}
-				sdkLog("remove file: ", v)
-			}
-			var sendMsgResp UserSendMsgResp
-			err = proto.Unmarshal(r.Data, &sendMsgResp)
-			if err != nil {
-				sdkLog("Unmarshal failed ", err.Error())
-			}
-			_ = u.updateMessageTimeAndMsgIDStatus(sendMsgResp.ClientMsgID, sendMsgResp.SendTime, MsgStatusSendSuccess)
-
-			s.ServerMsgID = sendMsgResp.ServerMsgID
-			s.SendTime = sendMsgResp.SendTime
-			s.Status = MsgStatusSendSuccess
-			c.LatestMsg = structToJsonString(s)
-			c.LatestMsgSendTime = s.SendTime
-			_ = u.triggerCmdUpdateConversation(updateConNode{conversationID, AddConOrUpLatMsg,
-				c})
-			_ = u.triggerCmdUpdateConversation(updateConNode{conversationID, ConChange, ""})
-		}
-		breakFlag = 1
-	case <-time.After(time.Second * time.Duration(timeout)):
-		var flag bool
-		sdkLog("ws ch recvMsg err: ", wsReq.OperationID)
-		if connSend != u.conn {
-			sdkLog("old conn != current conn  ", connSend, u.conn)
-			flag = false // error
-		} else {
-			flag = false //error
-			for tr := 0; tr < 3; tr++ {
-				err = u.sendPingMsg()
-				if err != nil {
-					sdkLog("sendPingMsg failed ", wsReq.OperationID, err.Error(), tr)
-					time.Sleep(time.Duration(30) * time.Second)
-				} else {
-					sdkLog("sendPingMsg ok ", wsReq.OperationID)
-					flag = true //wait continue
-					break
-				}
-			}
-		}
-		if flag == false {
-			callback.OnError(http.StatusRequestTimeout, http.StatusText(http.StatusRequestTimeout))
-			u.sendMessageFailedHandle(&s, &c, conversationID)
-			sdkLog("onError callback ", wsReq.OperationID)
-			breakFlag = 1
-			break
-		} else {
-			sdkLog("wait resp continue", wsReq.OperationID)
-			breakFlag = 0
-			continue
-		}
-	}
-
+	return ws.WaitResp(ch, timeout, wsReq.OperationID, connSend)
 }
