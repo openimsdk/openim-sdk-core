2a972163a8695ef0d07c56799cb231c006184243
Gordon <1432970085@qq.com>
2022-02-25 17:18:29 +0800

    test

--- test/sendMessage.go
+++ test/sendMessage.go
@@ -1,1 +1,43 @@
 package test
+
+import (
+	"github.com/gorilla/websocket"
+	"open_im_sdk/pkg/constant"
+	"open_im_sdk/pkg/log"
+	"open_im_sdk/pkg/utils"
+	"time"
+)
+
+func SendReqWaitResp(text, senderID, recvID, operationID string, conn *websocket.Conn) bool {
+	timeout := 300
+	retryTimes := 60
+	var wsReq GeneralWsReq
+	var connSend *websocket.Conn
+	var err error
+	wsReq.ReqIdentifier = constant.WSSendMsg
+	wsReq.OperationID = operationID
+	msgIncr, ch := w.AddCh(senderID)
+	defer w.DelCh(msgIncr)
+	wsReq.SendID = senderID
+	wsReq.MsgIncr = msgIncr
+	wsReq.Data, err = proto.Marshal(m)
+	if err != nil {
+		return nil, utils.Wrap(err, "proto marshal err")
+	}
+	for i := 0; i < retryTimes+1; i++ {
+		connSend, err = w.writeBinaryMsg(wsReq)
+		if err != nil {
+			if !w.IsWriteTimeout(err) {
+				log.Error(operationID, "Not send timeout, failed, close conn, writeBinaryMsg again ", err.Error())
+				w.CloseConn()
+				time.Sleep(time.Duration(1) * time.Second)
+				continue
+			} else {
+				return nil, utils.Wrap(err, "writeBinaryMsg timeout")
+			}
+		}
+		break
+	}
+	r1, r2 := w.WaitResp(ch, timeout, wsReq.OperationID, connSend)
+	return r1, r2
+}
