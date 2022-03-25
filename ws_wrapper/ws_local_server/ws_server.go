9951960b97c6dbdc4f88fe7344a30dd889cb23d6
Gordon <1432970085@qq.com>
2022-03-25 12:16:27 +0800

    ws modify

--- ws_wrapper/ws_local_server/ws_server.go
+++ ws_wrapper/ws_local_server/ws_server.go
@@ -83,24 +83,22 @@ func (ws *WServer) getMsgAndSend() {
 	for {
 		select {
 		case r := <-ws.ch:
-			go func() {
-				conns := ws.getUserConn(r.uid + " " + "Web")
-				if conns == nil {
-					wrapSdkLog("", "uid no conn, failed ", r.uid)
-				}
-				for _, conn := range conns {
-					if conn != nil {
-						wrapSdkLog("", "getMsgAndSend begin: ", string(r.data))
-						err := WS.writeMsg(conn, websocket.TextMessage, r.data)
-						wrapSdkLog("", "getMsgAndSend end: ", string(r.data))
-						if err != nil {
-							wrapSdkLog("", "WS WriteMsg error", "", "userIP", conn.RemoteAddr().String(), "userUid", r.uid, "error", err, "data", string(r.data))
-						}
-					} else {
-						wrapSdkLog("", "Conn is nil, failed", "data", string(r.data))
+			conns := ws.getUserConn(r.uid + " " + "Web")
+			if conns == nil {
+				wrapSdkLog("", "uid no conn, failed ", r.uid)
+			}
+			for _, conn := range conns {
+				if conn != nil {
+					wrapSdkLog("", "getMsgAndSend begin: ", string(r.data))
+					err := WS.writeMsg(conn, websocket.TextMessage, r.data)
+					wrapSdkLog("", "getMsgAndSend end: ", string(r.data))
+					if err != nil {
+						wrapSdkLog("", "WS WriteMsg error", "", "userIP", conn.RemoteAddr().String(), "userUid", r.uid, "error", err, "data", string(r.data))
 					}
+				} else {
+					wrapSdkLog("", "Conn is nil, failed", "data", string(r.data))
 				}
-			}()
+			}
 
 		}
 	}
