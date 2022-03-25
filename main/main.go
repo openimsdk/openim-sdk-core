4d47b68b040c99cc4c04073f87039b8e291a1b07
skiffer-git <44203734@qq.com>
2022-03-25 11:41:55 +0800

    test

--- main/main.go
+++ main/main.go
@@ -80,7 +80,7 @@ func main() {
 	//test.DoTestGetGroupHistoryMessage()
 	for {
 		test.DoTestSendMsg2(strMyUidx, test.Friend_uid)
-		time.Sleep(30 * time.Second)
+		time.Sleep(5 * time.Second)
 		log.Info("", "waiting...")
 	}
 	//reliabilityTest()
