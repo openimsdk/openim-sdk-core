347ac86fb729af88e5e856c74c25000959af3ba9
Gordon <1432970085@qq.com>
2022-04-07 18:27:59 +0800

    conversation update

--- internal/conversation_msg/conversation_msg.go
+++ internal/conversation_msg/conversation_msg.go
@@ -209,9 +209,10 @@ func (c *Conversation) doMsgNew(c2v common.Cmd2Value) {
 				if isConversationUpdate {
 					if isSenderConversationUpdate {
 						log.Debug(operationID, "updateConversation msg", v, lc)
-						c.updateConversation(&lc, conversationSet, false)
+						c.updateConversation(&lc, conversationSet)
 					} else {
-						c.updateConversation(&lc, conversationSet, true)
+						//special fix
+						c.updateConversation(&lc, conversationSet)
 					}
 					newMessages = append(newMessages, msg)
 				} else {
@@ -264,7 +265,7 @@ func (c *Conversation) doMsgNew(c2v common.Cmd2Value) {
 				}
 
 				if isConversationUpdate {
-					c.updateConversation(&lc, conversationSet, false)
+					c.updateConversation(&lc, conversationSet)
 					newMessages = append(newMessages, msg)
 				} else {
 					msg.Status = constant.MsgStatusFiltered
@@ -694,10 +695,7 @@ func (c *Conversation) msgHandleByContentType(msg *sdk_struct.MsgStruct) (err er
 
 	return err
 }
-func (c *Conversation) updateConversation(lc *db.LocalConversation, cs map[string]*db.LocalConversation, onlyUpdateTime bool) {
-	if onlyUpdateTime {
-		lc.LatestMsg = ""
-	}
+func (c *Conversation) updateConversation(lc *db.LocalConversation, cs map[string]*db.LocalConversation) {
 	if oldC, ok := cs[lc.ConversationID]; !ok {
 		cs[lc.ConversationID] = lc
 	} else {
