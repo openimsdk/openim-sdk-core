647c41730c12d4cbfd70f9bc4cb85c46017117b6
wenxu12345 <44203734@qq.com>
2022-04-26 10:44:57 +0800

    Verify token expired

--- internal/login/init_login.go
+++ internal/login/init_login.go
@@ -236,7 +236,8 @@ func (u *LoginMgr) login(userID, token string, cb open_im_sdk_callback.Base, ope
 	u.advancedFunction = sdk_advanced_function.NewChatHasRead(u.ws, u.loginUserID, u.db, u.imConfig.Platform, u.conversationCh, u.advancedMsgListener)
 	u.conversation = conv.NewConversation(u.ws, u.db, p, u.conversationCh,
 		u.loginUserID, u.imConfig.Platform, u.imConfig.DataDir,
-		u.friend, u.group, u.user, objStorage, u.conversationListener, u.advancedMsgListener, u.signaling, u.advancedFunction, u.organization, u.workMoments)
+		u.friend, u.group, u.user, objStorage, u.conversationListener, u.advancedMsgListener,
+		u.signaling, u.advancedFunction, u.organization, u.workMoments, u.cache)
 	u.conversation.SyncConversations(operationID)
 	go common.DoListener(u.conversation)
 	log.Info(operationID, "login success...")
