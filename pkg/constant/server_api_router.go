784da2360039694e8494390fc4445023be70d375
Bloomingg <1293499952@qq.com>
2022-04-27 14:24:49 +0800

    fix: user router

--- pkg/constant/server_api_router.go
+++ pkg/constant/server_api_router.go
@@ -4,7 +4,7 @@ const (
 	GetSelfUserInfoRouter       = "/user/get_self_user_info"
 	GetUsersInfoRouter          = "/user/get_users_info"
 	UpdateSelfUserInfoRouter    = "/user/update_user_info"
-	GetUsersInfoFromCacheRouter = "/get_users_info_from_cache"
+	GetUsersInfoFromCacheRouter = "/user/get_users_info_from_cache"
 
 	AddFriendRouter                    = "/friend/add_friend"
 	DeleteFriendRouter                 = "/friend/delete_friend"
