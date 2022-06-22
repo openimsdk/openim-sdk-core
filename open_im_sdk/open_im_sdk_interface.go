218e12cf29bfaaff439ae49a22482bace5717b6d
Gordon <1432970085@qq.com>
2022-06-22 16:41:00 +0800

    ws add group

--- open_im_sdk/open_im_sdk_interface.go
+++ open_im_sdk/open_im_sdk_interface.go
@@ -298,7 +298,7 @@ func GetGroupMemberList(callback open_im_sdk_callback.Base, operationID string, 
 	userForSDK.Group().GetGroupMemberList(callback, groupID, filter, offset, count, operationID)
 }
 
-func GetGroupMemberListByJoinTimeFilter(callback open_im_sdk_callback.Base, operationID string, groupID string, offset, count int32, joinTimeBegin, joinTimeEnd uint32) {
+func GetGroupMemberListByJoinTimeFilter(callback open_im_sdk_callback.Base, operationID string, groupID string, offset, count int32, joinTimeBegin, joinTimeEnd int32) {
 	if err := CheckResourceLoad(userForSDK); err != nil {
 		log.Error(operationID, "resource loading is not completed ", err.Error())
 		callback.OnError(constant.ErrResourceLoadNotComplete.ErrCode, constant.ErrResourceLoadNotComplete.ErrMsg)
