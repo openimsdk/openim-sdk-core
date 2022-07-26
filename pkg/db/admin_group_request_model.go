967aaf9a5af7fefa7e216707980cc47de26c1b80
wenxu12345 <44203734@qq.com>
2022-07-26 11:35:26 +0800

    group application sort by create time

--- pkg/db/admin_group_request_model.go
+++ pkg/db/admin_group_request_model.go
@@ -7,21 +7,14 @@ import (
 )
 
 func (d *DataBase) InsertAdminGroupRequest(groupRequest *model_struct.LocalAdminGroupRequest) error {
-	d.mRWMutex.Lock()
-	defer d.mRWMutex.Unlock()
 	return utils.Wrap(d.conn.Create(groupRequest).Error, "InsertAdminGroupRequest failed")
 }
 
 func (d *DataBase) DeleteAdminGroupRequest(groupID, userID string) error {
-	d.mRWMutex.Lock()
-	defer d.mRWMutex.Unlock()
 	return utils.Wrap(d.conn.Where("group_id=? and user_id=?", groupID, userID).Delete(&model_struct.LocalAdminGroupRequest{}).Error, "DeleteAdminGroupRequest failed")
 }
 
 func (d *DataBase) UpdateAdminGroupRequest(groupRequest *model_struct.LocalAdminGroupRequest) error {
-	d.mRWMutex.Lock()
-	defer d.mRWMutex.Unlock()
-
 	t := d.conn.Model(groupRequest).Select("*").Updates(*groupRequest)
 	if t.RowsAffected == 0 {
 		return utils.Wrap(errors.New("RowsAffected == 0"), "no update")
@@ -30,10 +23,8 @@ func (d *DataBase) UpdateAdminGroupRequest(groupRequest *model_struct.LocalAdmin
 }
 
 func (d *DataBase) GetAdminGroupApplication() ([]*model_struct.LocalAdminGroupRequest, error) {
-	d.mRWMutex.Lock()
-	defer d.mRWMutex.Unlock()
 	var groupRequestList []model_struct.LocalAdminGroupRequest
-	err := utils.Wrap(d.conn.Find(&groupRequestList).Error, "")
+	err := utils.Wrap(d.conn.Order("create_time DESC").Find(&groupRequestList).Error, "")
 	if err != nil {
 		return nil, utils.Wrap(err, "")
 	}
--- pkg/db/group_request_model.go
+++ pkg/db/group_request_model.go
@@ -7,20 +7,12 @@ import (
 )
 
 func (d *DataBase) InsertGroupRequest(groupRequest *model_struct.LocalGroupRequest) error {
-	d.mRWMutex.Lock()
-	defer d.mRWMutex.Unlock()
 	return utils.Wrap(d.conn.Create(groupRequest).Error, "InsertGroupRequest failed")
-
 }
 func (d *DataBase) DeleteGroupRequest(groupID, userID string) error {
-	d.mRWMutex.Lock()
-	defer d.mRWMutex.Unlock()
 	return utils.Wrap(d.conn.Where("group_id=? and user_id=?", groupID, userID).Delete(&model_struct.LocalGroupRequest{}).Error, "DeleteGroupRequest failed")
 }
 func (d *DataBase) UpdateGroupRequest(groupRequest *model_struct.LocalGroupRequest) error {
-	d.mRWMutex.Lock()
-	defer d.mRWMutex.Unlock()
-
 	t := d.conn.Model(groupRequest).Select("*").Updates(*groupRequest)
 	if t.RowsAffected == 0 {
 		return utils.Wrap(errors.New("RowsAffected == 0"), "no update")
@@ -28,37 +20,9 @@ func (d *DataBase) UpdateGroupRequest(groupRequest *model_struct.LocalGroupReque
 	return utils.Wrap(t.Error, "")
 }
 
-//
-//func (d *DataBase) GetAdminGroupApplication() ([]*LocalGroupRequest, error) {
-//	ownerAdminList, err := d.GetGroupMenberInfoIfOwnerOrAdmin()
-//	if err != nil {
-//		return nil, utils.Wrap(err, "")
-//	}
-//
-//	//fmt.Println("ownerAdminList ", ownerAdminList)
-//	d.mRWMutex.Lock()
-//	defer d.mRWMutex.Unlock()
-//	var transfer []*LocalGroupRequest
-//	for _, v := range ownerAdminList {
-//		var groupRequest LocalGroupRequest
-//		f := d.conn.Where("group_id = ?", v.GroupID).Find(&groupRequest)
-//		err := f.Error
-//
-//		if err != nil {
-//			continue
-//		}
-//		if f.RowsAffected != 0 {
-//			transfer = append(transfer, &groupRequest)
-//		}
-//	}
-//	return transfer, utils.Wrap(err, "GetRecvGroupApplication failed ")
-//}
-
 func (d *DataBase) GetSendGroupApplication() ([]*model_struct.LocalGroupRequest, error) {
-	d.mRWMutex.Lock()
-	defer d.mRWMutex.Unlock()
 	var groupRequestList []model_struct.LocalGroupRequest
-	err := utils.Wrap(d.conn.Find(&groupRequestList).Error, "")
+	err := utils.Wrap(d.conn.Order("create_time DESC").Find(&groupRequestList).Error, "")
 	if err != nil {
 		return nil, utils.Wrap(err, "")
 	}
