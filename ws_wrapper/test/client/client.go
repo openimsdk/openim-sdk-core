c1a697b6b79cfa26372ad4db7cc8735d2f3785c0
wangchuxiao <wangchuxiao97@outlook.com>
2022-08-19 00:47:18 +0800

    ws test

---
+++ ws_wrapper/test/client/client.go
@@ -0,0 +1,133 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"open_im_sdk/pkg/network"
+	"open_im_sdk/pkg/server_api_params"
+	"open_im_sdk/pkg/utils"
+
+	"github.com/gorilla/websocket"
+)
+
+type IMClient struct {
+	Token    string
+	UserID   string
+	ApiURL   string
+	JssdkURL string
+	Platform int
+	Conn     *websocket.Conn
+}
+
+func NewIMClient(token, userID, apiURL, jssdkURL string, platform int) *IMClient {
+	return &IMClient{
+		Token:    token,
+		UserID:   userID,
+		ApiURL:   apiURL,
+		JssdkURL: jssdkURL,
+		Platform: platform,
+	}
+}
+
+func (i *IMClient) GetToken() (string, error) {
+	req := struct {
+		Secret      string `json:"secret"`
+		Platform    int32  `json:"platform"`
+		UserID      string `json:"userID"`
+		OperationID string `json:"operationID"`
+	}{Secret: "tuoyun", Platform: int32(i.Platform), UserID: i.UserID, OperationID: utils.OperationIDGenerator()}
+	content, err := network.Post2Api(i.ApiURL+"/auth/user_token", req, "")
+	if err != nil {
+		return "", err
+	}
+	type respToken struct {
+		Data struct {
+			ExpiredTime int64  `json:"expiredTime"`
+			Token       string `json:"token"`
+			Uid         string `json:"uid"`
+		}
+		ErrCode int    `json:"errCode"`
+		ErrMsg  string `json:"errMsg"`
+	}
+	resp := respToken{}
+	err = json.Unmarshal(content, &resp)
+	return resp.Data.Token, err
+}
+
+func (i *IMClient) GetUserIDList() ([]string, error) {
+	req := struct {
+		OperationID string `json:"operationID"`
+	}{OperationID: utils.OperationIDGenerator()}
+	content, err := network.Post2Api(i.ApiURL+"/user/get_all_users_uid", req, i.Token)
+	if err != nil {
+		return nil, err
+	}
+	type Resp struct {
+		UserIDList []string `json:"data"`
+	}
+	resp := Resp{}
+	err = json.Unmarshal(content, &resp)
+	return resp.UserIDList, err
+}
+
+func (i *IMClient) SendTestMsg(conn *websocket.Conn) error {
+	return conn.WriteMessage(1, []byte("s"))
+}
+
+func (i *IMClient) WsLogin() error {
+	loginContent := struct {
+		UserID string `json:"userID"`
+		Token  string `json:"token"`
+	}{UserID: i.UserID, Token: i.Token}
+	return i.Conn.WriteMessage(1, i.getWsReq("Login", 1, loginContent))
+}
+
+func (i *IMClient) WsLogout() error {
+	return i.Conn.WriteMessage(1, i.getWsReq("Logout", 1, nil))
+}
+
+func (i *IMClient) GetLoginStatus() error {
+	return i.Conn.WriteMessage(1, i.getWsReq("GetLoginStatus", 0, nil))
+}
+
+func (i *IMClient) SendMsg(userID string) error {
+	msg := server_api_params.MsgData{
+		SendID:           i.UserID,
+		RecvID:           "MTc3MjYzNzg0Mjg=",
+		SenderPlatformID: int32(i.Platform),
+		ClientMsgID:      utils.GetMsgID(i.UserID),
+		CreateTime:       utils.GetCurrentTimestampByMill(),
+		SessionType:      1,
+		MsgFrom:          100,
+		ContentType:      101,
+	}
+	return i.Conn.WriteMessage(1, i.getWsReq("SendMessage", 1, msg))
+}
+
+func (i *IMClient) GetSelfUserInfo() error {
+	return i.Conn.WriteMessage(1, i.getWsReq("GetSelfUserInfo", 0, nil))
+}
+
+func (i *IMClient) getWsReq(event string, batch int, data interface{}) []byte {
+	type Req struct {
+		ReqFuncName string `json:"reqFuncName" `
+		OperationID string `json:"operationID"`
+		Data        string `json:"data"`
+		UserID      string `json:"userID"`
+		Batch       int    `json:"batchMsg,omitempty"`
+	}
+	req := Req{
+		ReqFuncName: event,
+		OperationID: i.UserID + utils.OperationIDGenerator(),
+		UserID:      i.UserID,
+	}
+	req.Batch = batch
+	var bytes []byte
+	if data != nil {
+		bytes, _ := json.Marshal(data)
+		req.Data = string(bytes)
+	}
+	bytes, _ = json.Marshal(req)
+	fmt.Println(string(bytes))
+	return bytes
+}
---
+++ ws_wrapper/test/simulationClient.go
@@ -0,0 +1,58 @@
+package test
+
+import (
+	"log"
+	"net/url"
+	"open_im_sdk/pkg/utils"
+	"open_im_sdk/ws_wrapper/test/client"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var totalConnNum int
+var lock sync.Mutex
+
+func StartSimulationJSClient(api, jssdkURL, userID string, num int, userIDList []string) {
+	user := client.NewIMClient("", userID, api, jssdkURL, 1)
+	var err error
+	user.Token, err = user.GetToken()
+	if err != nil {
+		log.Println("generate token failed", userID, api, err.Error())
+	}
+	v := url.Values{}
+	v.Set("sendID", userID)
+	v.Set("token", user.Token)
+	v.Set("platformID", utils.IntToString(1))
+	c, _, err := websocket.DefaultDialer.Dial(jssdkURL+"?"+v.Encode(), nil)
+	if err != nil {
+		log.Println("dial:", err.Error(), "userID", userID, "num: ", num)
+		return
+	}
+	lock.Lock()
+	totalConnNum += 1
+	log.Println("connect success", userID, "total conn num", totalConnNum)
+	user.Conn = c
+	lock.Unlock()
+	user.WsLogin()
+	user.GetSelfUserInfo()
+	user.GetLoginStatus()
+	go func() {
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err, "error an connet failed", userID)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	go func() {
+		for {
+			user.SendMsg(userID)
+			time.Sleep(time.Second * 1)
+		}
+	}()
+}
