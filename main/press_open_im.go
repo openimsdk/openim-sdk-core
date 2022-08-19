5750928543f5afe17044c26e2b2426db419cff9a
skiffer-git <44203734@qq.com>
2022-08-19 17:05:08 +0800

    press

--- main/press_open_im.go
+++ main/press_open_im.go
@@ -11,14 +11,13 @@ func main() {
 	var senderNum *int          //Number of users sending messages
 	var singleSenderMsgNum *int //Number of single user send messages
 	var intervalTime *int       //Sending time interval, in millisecond
-	senderNum = flag.Int("sn", 2, "sender num")
-	singleSenderMsgNum = flag.Int("mn", 10000, "single sender msg num")
-	intervalTime = flag.Int("t", 10, "interval time mill second")
+	senderNum = flag.Int("sn", 100, "sender num")
+	singleSenderMsgNum = flag.Int("mn", 100, "single sender msg num")
+	intervalTime = flag.Int("t", 1, "interval time mill second")
 	flag.Parse()
 	constant.OnlyForTest = 1
 	log.NewPrivateLog("", uint32(test.LogLevel))
 	log.Warn("", "press test begin, sender num: ", *senderNum, " single sender msg num: ", *singleSenderMsgNum, " send msg total num: ", *senderNum**singleSenderMsgNum)
 	test.PressTest(*singleSenderMsgNum, *intervalTime, *senderNum)
-	log.Warn("", "press test end, sender num: ", *senderNum, " single sender msg num: ", *singleSenderMsgNum, " send msg total num: ", *senderNum**singleSenderMsgNum)
 	select {}
 }
