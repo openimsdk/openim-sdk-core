64d8ddf55f7a29381152cfd1064a75a1e23f2b7f
Gordon <1432970085@qq.com>
2022-08-29 15:39:46 +0800

    db init change

--- pkg/db/db_init.go
+++ pkg/db/db_init.go
@@ -101,8 +101,9 @@ func (d *DataBase) initDB() error {
 		return utils.Wrap(err, "get sql db failed")
 	}
 	sqlDB.SetConnMaxLifetime(time.Hour * 1)
-	sqlDB.SetMaxOpenConns(2)
-	sqlDB.SetMaxIdleConns(1)
+	sqlDB.SetMaxOpenConns(3)
+	sqlDB.SetMaxIdleConns(2)
+	sqlDB.SetConnMaxIdleTime(time.Minute * 10)
 	d.conn = db
 
 	superGroup := &model_struct.LocalGroup{}
