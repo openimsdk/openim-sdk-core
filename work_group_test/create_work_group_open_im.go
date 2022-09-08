861cc1723bc1f58ef425915f87eb1117975d7f30
skiffer-git <44203734@qq.com>
2022-09-08 10:01:00 +0800

    press

--- work_group_test/create_work_group_open_im.go
+++ work_group_test/create_work_group_open_im.go
@@ -8,11 +8,13 @@ import (
 
 func main() {
 	var groupMemberNumber *int
-	groupMemberNumber = flag.Int("gmn", 10, "group member number ")
+	groupMemberNumber = flag.Int("gmn", 1000, "group member number ")
 	flag.Parse()
 	log.NewPrivateLog("", test.LogLevel)
 	log.Warn("", "CreateWorkGroup  start, group member number: ", *groupMemberNumber)
 	*groupMemberNumber = *groupMemberNumber + 2
+
 	test.CreateWorkGroup(*groupMemberNumber)
 	log.Warn("", "CreateWorkGroup finish, group member number: ", *groupMemberNumber+1)
+
 }
