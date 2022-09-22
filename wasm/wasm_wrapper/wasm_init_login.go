ce6c5741fa8629cac6a761feedcd79a727b53223
Gordon <1432970085@qq.com>
2022-09-22 17:42:11 +0800

    log

--- wasm/wasm_wrapper/wasm_init_login.go
+++ wasm/wasm_wrapper/wasm_init_login.go
@@ -1,15 +1,26 @@
 package wasm_wrapper
 
 import (
+	"errors"
 	"open_im_sdk/open_im_sdk"
+	"open_im_sdk/open_im_sdk_callback"
 	"open_im_sdk/pkg/utils"
 	"open_im_sdk/wasm/event_listener"
+	"runtime"
 	"syscall/js"
 )
 
 const COMMONEVENTFUNC = "commonEventFunc"
 
 var commonFunc *js.Value
+var ErrArgsLength = errors.New("from javascript args length err")
+
+func checker(callback open_im_sdk_callback.Base, args *[]js.Value, count int) {
+	if len(*args) != count {
+		callback.OnError(100, ErrArgsLength.Error())
+		runtime.Goexit()
+	}
+}
 
 func CommonEventFunc(_ js.Value, args []js.Value) interface{} {
 	if len(args) >= 1 {
@@ -26,10 +37,7 @@ func InitSDK(_ js.Value, args []js.Value) interface{} {
 }
 func Login(_ js.Value, args []js.Value) interface{} {
 	callback := event_listener.NewBaseCallback(utils.FirstLower(utils.GetSelfFuncName()), args[0].String(), commonFunc)
-	if len(args) < 3 {
-		callback.OnError(100, "args err")
-		return nil
-	}
+	checker(callback, &args, 3)
 	open_im_sdk.Login(callback, args[0].String(), args[1].String(), args[2].String())
 	return nil
 }
