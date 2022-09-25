e188062b7edb30e3a2b5635cb04add86c65dfc92
Gordon <1432970085@qq.com>
2022-09-25 15:05:02 +0800

    wasm update

---
+++ wasm/event_listener/callback_writer.go
@@ -0,0 +1,12 @@
+package event_listener
+
+type CallbackWriter interface {
+	SendMessage()
+	SetEvent(event string) CallbackWriter
+	SetData(data interface{}) CallbackWriter
+	SetErrCode(errCode int32) CallbackWriter
+	SetOperationID(operationID string) CallbackWriter
+	SetErrMsg(errMsg string) CallbackWriter
+	SetSelfCallerFuncName() CallbackWriter
+	GetOperationID() string
+}
