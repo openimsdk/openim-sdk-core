09ffcb66d45f21e5d120e6f2b3206c60c44f89ca
skiffer-git <44203734@qq.com>
2022-10-07 20:50:12 +0800

    for test

--- single_test/press_open_im.go
+++ single_test/press_open_im.go
@@ -12,9 +12,9 @@ func main() {
 	var senderNum *int          //Number of users sending messages
 	var singleSenderMsgNum *int //Number of single user send messages
 	var intervalTime *int       //Sending time interval, in millisecond
-	senderNum = flag.Int("sn", 100, "sender num")
-	singleSenderMsgNum = flag.Int("mn", 100, "single sender msg num")
-	intervalTime = flag.Int("t", 100, "interval time mill second")
+	senderNum = flag.Int("sn", 10, "sender num")
+	singleSenderMsgNum = flag.Int("mn", 10, "single sender msg num")
+	intervalTime = flag.Int("t", 1000, "interval time mill second")
 	flag.Parse()
 	constant.OnlyForTest = 1
 	log.NewPrivateLog("", test.LogLevel)
