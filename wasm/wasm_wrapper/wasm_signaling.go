7bdd5903672c6b3a1778e2f314d852ec66a80229
wangchuxiao <wangchuxiao97@outlook.com>
2022-12-14 11:28:01 +0800

    wasm add signaling

--- wasm/wasm_wrapper/wasm_signaling.go
+++ wasm/wasm_wrapper/wasm_signaling.go
@@ -45,3 +45,13 @@ func (w *WrapperSignaling) SignalingHungUp(_ js.Value, args []js.Value) interfac
 	callback := event_listener.NewBaseCallback(utils.FirstLower(utils.GetSelfFuncName()), w.commonFunc)
 	return event_listener.NewCaller(open_im_sdk.SignalingHungUp, callback, &args).AsyncCallWithCallback()
 }
+
+func (w *WrapperSignaling) SignalingGetRoomByGroupID(_ js.Value, args []js.Value) interface{} {
+	callback := event_listener.NewBaseCallback(utils.FirstLower(utils.GetSelfFuncName()), w.commonFunc)
+	return event_listener.NewCaller(open_im_sdk.SignalingGetRoomByGroupID, callback, &args).AsyncCallWithCallback()
+}
+
+func (w *WrapperSignaling) SignalingGetTokenByRoomID(_ js.Value, args []js.Value) interface{} {
+	callback := event_listener.NewBaseCallback(utils.FirstLower(utils.GetSelfFuncName()), w.commonFunc)
+	return event_listener.NewCaller(open_im_sdk.SignalingGetTokenByRoomID, callback, &args).AsyncCallWithCallback()
+}
