fecb3db32aa044cd6a0acb517052e9dbe062f7ac
withchao <993506633@qq.com>
2023-04-14 16:54:10 +0800

    call

--- pkg/common/diff.go
+++ pkg/common/diff.go
@@ -2,11 +2,9 @@ package common
 
 import (
 	"fmt"
-	"open_im_sdk/pkg/db/model_struct"
-	"time"
-
 	"github.com/google/go-cmp/cmp"
 	"github.com/jinzhu/copier"
+	"open_im_sdk/pkg/db/model_struct"
 
 	//log2 "open_im_sdk/pkg/log"
 	"open_im_sdk/pkg/server_api_params"
@@ -180,8 +178,6 @@ func SendGroupRequestCopyToLocal(dst *model_struct.LocalGroupRequest, src *serve
 func TransferToLocalUserInfo(apiData *server_api_params.UserInfo) *model_struct.LocalUser {
 	var localNode model_struct.LocalUser
 	copier.Copy(&localNode, apiData)
-	t, _ := time.Parse("2006-01-02", apiData.BirthStr)
-	localNode.BirthTime = t
 	localNode.AppMangerLevel = 0
 	return &localNode
 }
@@ -431,190 +427,6 @@ func CheckGroupMemberDiff(a []*model_struct.LocalGroupMember, b []*model_struct.
 	return aInBNot, bInANot, sameA, sameB
 }
 
-func CheckDepartmentMemberDiff(a []*model_struct.LocalDepartmentMember, b []*model_struct.LocalDepartmentMember) (aInBNot, bInANot, sameA, sameB []int) {
-	//to map, friendid_>friendinfo
-	mapA := make(map[string]*model_struct.LocalDepartmentMember)
-	for _, v := range a {
-		mapA[v.DepartmentID+v.UserID] = v
-	}
-	mapB := make(map[string]*model_struct.LocalDepartmentMember)
-	for _, v := range b {
-		mapB[v.DepartmentID+v.UserID] = v
-	}
-
-	aInBNot = make([]int, 0)
-	bInANot = make([]int, 0)
-	sameA = make([]int, 0)
-	sameB = make([]int, 0)
-
-	//for a
-	for i, v := range a {
-		ia, ok := mapB[v.DepartmentID+v.UserID]
-		if !ok {
-			//in a, but not in b
-			aInBNot = append(aInBNot, i)
-		} else {
-			//reflect.DeepEqual(a, b)
-			//	reflect.DeepEqual(v, ia)
-			//if !cmp.Equal(v, ia)
-			if !cmp.Equal(v, ia) {
-				// key of a and b is equal, but value different
-				sameA = append(sameA, i)
-			}
-		}
-	}
-	//for b
-	for i, v := range b {
-		ib, ok := mapA[v.DepartmentID+v.UserID]
-		if !ok {
-			bInANot = append(bInANot, i)
-		} else {
-			if !cmp.Equal(v, ib) {
-				sameB = append(sameB, i)
-			}
-		}
-	}
-	return aInBNot, bInANot, sameA, sameB
-
-}
-
-func CheckDepartmentDiff(a []*model_struct.LocalDepartment, b []*model_struct.LocalDepartment) (aInBNot, bInANot, sameA, sameB []int) {
-	//to map, friendid_>friendinfo
-	mapA := make(map[string]*model_struct.LocalDepartment)
-	for _, v := range a {
-		mapA[v.DepartmentID] = v
-	}
-	mapB := make(map[string]*model_struct.LocalDepartment)
-	for _, v := range b {
-		mapB[v.DepartmentID] = v
-	}
-
-	aInBNot = make([]int, 0)
-	bInANot = make([]int, 0)
-	sameA = make([]int, 0)
-	sameB = make([]int, 0)
-
-	//for a
-	for i, v := range a {
-		ia, ok := mapB[v.DepartmentID]
-		if !ok {
-			//in a, but not in b
-			aInBNot = append(aInBNot, i)
-		} else {
-			if !cmp.Equal(v, ia) {
-				// key of a and b is equal, but value different
-				sameA = append(sameA, i)
-			}
-		}
-	}
-	//for b
-	for i, v := range b {
-		ib, ok := mapA[v.DepartmentID]
-		if !ok {
-			bInANot = append(bInANot, i)
-		} else {
-			if !cmp.Equal(v, ib) {
-				sameB = append(sameB, i)
-			}
-		}
-	}
-	return aInBNot, bInANot, sameA, sameB
-
-}
-
-func CheckGroupRequestDiff(a []*model_struct.LocalGroupRequest, b []*model_struct.LocalGroupRequest) (aInBNot, bInANot, sameA, sameB []int) {
-	//to map, friendid_>friendinfo
-	mapA := make(map[string]*model_struct.LocalGroupRequest)
-	for _, v := range a {
-		mapA[v.GroupID+v.UserID] = v
-	}
-	mapB := make(map[string]*model_struct.LocalGroupRequest)
-	for _, v := range b {
-		mapB[v.GroupID+v.UserID] = v
-	}
-
-	aInBNot = make([]int, 0)
-	bInANot = make([]int, 0)
-	sameA = make([]int, 0)
-	sameB = make([]int, 0)
-
-	//for a
-	for i, v := range a {
-		ia, ok := mapB[v.GroupID+v.UserID]
-		if !ok {
-			//in a, but not in b
-			aInBNot = append(aInBNot, i)
-		} else {
-			if !cmp.Equal(v, ia) {
-				// key of a and b is equal, but value different
-				sameA = append(sameA, i)
-			}
-		}
-	}
-	//for b
-	for i, v := range b {
-		ib, ok := mapA[v.GroupID+v.UserID]
-		if !ok {
-			bInANot = append(bInANot, i)
-		} else {
-			if !cmp.Equal(v, ib) {
-				sameB = append(sameB, i)
-			}
-		}
-	}
-	return aInBNot, bInANot, sameA, sameB
-}
-
-func CheckAdminGroupRequestDiff(a []*model_struct.LocalAdminGroupRequest, b []*model_struct.LocalAdminGroupRequest) (aInBNot, bInANot, sameA, sameB []int) {
-	//to map, friendid_>friendinfo
-	mapA := make(map[string]*model_struct.LocalAdminGroupRequest)
-	for _, v := range a {
-		mapA[v.GroupID+v.UserID] = v
-		//	fmt.Println("mapA   ", v)
-	}
-	mapB := make(map[string]*model_struct.LocalAdminGroupRequest)
-	for _, v := range b {
-		mapB[v.GroupID+v.UserID] = v
-		//	fmt.Println("mapB   ", v)
-	}
-
-	aInBNot = make([]int, 0)
-	bInANot = make([]int, 0)
-	sameA = make([]int, 0)
-	sameB = make([]int, 0)
-
-	//for a
-	for i, v := range a {
-		ia, ok := mapB[v.GroupID+v.UserID]
-		if !ok {
-			//in a, but not in b
-			//fmt.Println("aInBNot", a[i], ia)
-			aInBNot = append(aInBNot, i)
-		} else {
-			if !cmp.Equal(v, ia) {
-				// key of a and b is equal, but value different
-				//fmt.Println("sameA", a[i], ia)
-				sameA = append(sameA, i)
-			}
-		}
-	}
-	//for b
-	for i, v := range b {
-		ib, ok := mapA[v.GroupID+v.UserID]
-		if !ok {
-			//fmt.Println("bInANot", b[i], ib)
-
-			bInANot = append(bInANot, i)
-		} else {
-			if !cmp.Equal(v, ib) {
-				//	fmt.Println("sameB", b[i], ib)
-				sameB = append(sameB, i)
-			}
-		}
-	}
-	return aInBNot, bInANot, sameA, sameB
-}
-
 func CheckConversationListDiff(conversationsOnServer, conversationsOnLocal []*tempConversation) (aInBNot, bInANot, sameA, sameB []int) {
 	mapA := make(map[string]*tempConversation)
 	mapB := make(map[string]*tempConversation)
@@ -708,102 +520,6 @@ func CheckReactionExtensionsDiff(onServer, onLocal []*server_api_params.SingleMe
 	return aInBNot, bInANot, sameA, sameB
 }
 
-//
-//func CheckSendGroupRequestDiff(a []*db.LocalGroupRequest, b []*db.LocalGroupRequest) (aInBNot, bInANot, sameA, sameB []int) {
-//	//to map, friendid_>friendinfo
-//	mapA := make(map[string]*db.LocalGroupRequest)
-//	for _, v := range a {
-//		mapA[v.GroupID+v.UserID] = v
-//		fmt.Println("mapA   ", v)
-//	}
-//	mapB := make(map[string]*db.LocalGroupRequest)
-//	for _, v := range b {
-//		mapB[v.GroupID+v.UserID] = v
-//		fmt.Println("mapB   ", v)
-//	}
-//
-//	aInBNot = make([]int, 0)
-//	bInANot = make([]int, 0)
-//	sameA = make([]int, 0)
-//	sameB = make([]int, 0)
-//
-//	//for a
-//	for i, v := range a {
-//		ia, ok := mapB[v.GroupID+v.UserID]
-//		if !ok {
-//			//in a, but not in b
-//			fmt.Println("aInBNot", a[i], ia)
-//			aInBNot = append(aInBNot, i)
-//		} else {
-//			if !cmp.Equal(v, ia) {
-//				// key of a and b is equal, but value different
-//				fmt.Println("sameA", a[i], ia)
-//				sameA = append(sameA, i)
-//			}
-//		}
-//	}
-//	//for b
-//	for i, v := range b {
-//		ib, ok := mapA[v.GroupID+v.UserID]
-//		if !ok {
-//			fmt.Println("bInANot", b[i], ib)
-//
-//			bInANot = append(bInANot, i)
-//		} else {
-//			if !cmp.Equal(v, ib) {
-//				fmt.Println("sameB", b[i], ib)
-//				sameB = append(sameB, i)
-//			}
-//		}
-//	}
-//	return aInBNot, bInANot, sameA, sameB
-//}
-
-func TransferToLocalAdminGroupRequest(apiData []*server_api_params.GroupRequest) []*model_struct.LocalAdminGroupRequest {
-	local := make([]*model_struct.LocalAdminGroupRequest, 0)
-	//operationID := utils.OperationIDGenerator()
-	for _, v := range apiData {
-		var node model_struct.LocalAdminGroupRequest
-		//	log2.NewDebug(operationID, "local test api ", v)
-		AdminGroupRequestCopyToLocal(&node, v)
-		//		log2.NewDebug(operationID, "local test local  ", node)
-		local = append(local, &node)
-	}
-	//	log2.NewDebug(operationID, "local test local all ", local)
-	return local
-}
-
-func TransferToLocalDepartmentMember(apiData []*server_api_params.UserDepartmentMember) []*model_struct.LocalDepartmentMember {
-	local := make([]*model_struct.LocalDepartmentMember, 0)
-	for _, v := range apiData {
-		var node model_struct.LocalDepartmentMember
-		copier.Copy(&node, v.DepartmentMember)
-		copier.Copy(&node, v.OrganizationUser)
-		local = append(local, &node)
-	}
-	return local
-}
-
-func TransferToLocalDepartment(apiData []*server_api_params.Department) []*model_struct.LocalDepartment {
-	local := make([]*model_struct.LocalDepartment, 0)
-	for _, v := range apiData {
-		var node model_struct.LocalDepartment
-		copier.Copy(&node, v)
-		local = append(local, &node)
-	}
-	return local
-}
-
-func TransferToLocalSendGroupRequest(apiData []*server_api_params.GroupRequest) []*model_struct.LocalGroupRequest {
-	local := make([]*model_struct.LocalGroupRequest, 0)
-	for _, v := range apiData {
-		var node model_struct.LocalGroupRequest
-		SendGroupRequestCopyToLocal(&node, v)
-		local = append(local, &node)
-	}
-	return local
-}
-
 type tempConversation struct {
 	RecvMsgOpt            int32
 	ConversationID        string
@@ -888,22 +604,4 @@ func TransferToLocalConversation(resp server_api_params.GetAllConversationsResp)
 		})
 	}
 	return localConversations
-}
-
-func TransferToServerConversation(local []*model_struct.LocalConversation) server_api_params.GetAllConversationsResp {
-	var serverConversations server_api_params.GetAllConversationsResp
-	for _, localConversation := range local {
-		serverConversations.Conversations = append(serverConversations.Conversations, server_api_params.Conversation{
-			RecvMsgOpt:       localConversation.RecvMsgOpt,
-			ConversationID:   localConversation.ConversationID,
-			ConversationType: localConversation.ConversationType,
-			UserID:           localConversation.UserID,
-			GroupID:          localConversation.GroupID,
-			IsPrivateChat:    localConversation.IsPrivateChat,
-			IsPinned:         localConversation.IsPinned,
-			AttachedInfo:     localConversation.AttachedInfo,
-			Ex:               localConversation.Ex,
-		})
-	}
-	return serverConversations
 }
