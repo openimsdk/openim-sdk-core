cbd5d2ed6f9e43479de5f3057f05748b48581632
withchao <993506633@qq.com>
2023-04-27 16:25:24 +0800

    context

--- pkg/ccontext/context.go
+++ pkg/ccontext/context.go
@@ -4,10 +4,9 @@ import (
 	"context"
 )
 
-type Info struct {
+type GlobalConfig struct {
 	UserID               string
 	Token                string
-	OperationID          string
 	Platform             string
 	ApiAddr              string
 	WsAddr               string
@@ -19,12 +18,90 @@ type Info struct {
 	IsExternalExtensions bool
 }
 
-type infoKey struct{}
-
-func WithInfo(ctx context.Context, info *Info) context.Context {
-	return context.WithValue(ctx, infoKey{}, &info)
+type ContextInfo interface {
+	UserID() string
+	Token() string
+	Platform() string
+	ApiAddr() string
+	WsAddr() string
+	DataDir() string
+	LogLevel() string
+	ObjectStorage() string
+	EncryptionKey() string
+	OperationID() string
+	IsCompression() bool
+	IsExternalExtensions() bool
 }
 
-func GetCtxInfo(ctx context.Context) Info {
-	return *ctx.Value(infoKey{}).(*Info)
+func Info(ctx context.Context) ContextInfo {
+	conf := ctx.Value(globalConfigKey{}).(*GlobalConfig)
+	return &info{
+		conf: conf,
+		ctx:  ctx,
+	}
+}
+
+func WithInfo(ctx context.Context, conf *GlobalConfig) context.Context {
+	return context.WithValue(ctx, globalConfigKey{}, conf)
+}
+
+func WithOperationID(ctx context.Context, operationID string) context.Context {
+	return context.WithValue(ctx, operationIDKey{}, operationID)
+}
+
+type globalConfigKey struct{}
+
+type operationIDKey struct{}
+
+type info struct {
+	conf *GlobalConfig
+	ctx  context.Context
+}
+
+func (i *info) UserID() string {
+	return i.conf.UserID
+}
+
+func (i *info) Token() string {
+	return i.conf.Token
+}
+
+func (i *info) Platform() string {
+	return i.conf.Platform
+}
+
+func (i *info) ApiAddr() string {
+	return i.conf.ApiAddr
+}
+
+func (i *info) WsAddr() string {
+	return i.conf.WsAddr
+}
+
+func (i *info) DataDir() string {
+	return i.conf.DataDir
+}
+
+func (i *info) LogLevel() string {
+	return i.conf.LogLevel
+}
+
+func (i *info) ObjectStorage() string {
+	return i.conf.ObjectStorage
+}
+
+func (i *info) EncryptionKey() string {
+	return i.conf.EncryptionKey
+}
+
+func (i *info) OperationID() string {
+	return i.ctx.Value(operationIDKey{}).(string)
+}
+
+func (i *info) IsCompression() bool {
+	return i.conf.IsCompression
+}
+
+func (i *info) IsExternalExtensions() bool {
+	return i.conf.IsExternalExtensions
 }
