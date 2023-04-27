93674f64175e2f8e3ac75e581a5fdb88b79845f0
withchao <993506633@qq.com>
2023-04-27 15:53:47 +0800

    client

--- pkg/ccontext/context.go
+++ pkg/ccontext/context.go
@@ -5,6 +5,8 @@ import (
 )
 
 type Info struct {
+	UserID               string
+	Token                string
 	OperationID          string
 	Platform             string
 	ApiAddr              string
@@ -17,170 +19,12 @@ type Info struct {
 	IsExternalExtensions bool
 }
 
-type baseInfo struct{}
+type infoKey struct{}
 
 func WithInfo(ctx context.Context, info *Info) context.Context {
-	return context.WithValue(ctx, baseInfo{}, &info)
+	return context.WithValue(ctx, infoKey{}, &info)
 }
 
 func GetCtxInfo(ctx context.Context) Info {
-	return *ctx.Value(baseInfo{}).(*Info)
+	return *ctx.Value(infoKey{}).(*Info)
 }
-
-//const (
-//	Platform             = "platform"
-//	ApiAddr              = "apiAddr"
-//	WsAddr               = "wsAddr"
-//	DataDir              = "dataDir"
-//	LogLevel             = "logLevel"
-//	ObjectStorage        = "objectStorage"
-//	EncryptionKey        = "encryptionKey"
-//	IsCompression        = "isCompression"
-//	IsExternalExtensions = "isExternalExtensions"
-//)
-//
-//var keys = map[string]struct{}{
-//	Platform:             {},
-//	ApiAddr:              {},
-//	WsAddr:               {},
-//	DataDir:              {},
-//	LogLevel:             {},
-//	ObjectStorage:        {},
-//	EncryptionKey:        {},
-//	IsCompression:        {},
-//	IsExternalExtensions: {},
-//}
-
-//var mapper = []string{Platform, ApiAddr, WsAddr, DataDir, LogLevel,
-//	ObjectStorage, EncryptionKey, IsCompression, IsExternalExtensions}
-//func WithOpUserIDContext(ctx context.Context, opUserID string) context.Context {
-//	return context.WithValue(ctx, constant.OpUserID, opUserID)
-//}
-//func WithOpUserPlatformContext(ctx context.Context, platform string) context.Context {
-//	return context.WithValue(ctx, constant.OpUserPlatform, platform)
-//}
-//func WithTriggerIDContext(ctx context.Context, triggerID string) context.Context {
-//	return context.WithValue(ctx, constant.TriggerID, triggerID)
-//}
-
-//func NewCtx(operationID string) context.Context {
-//	c := context.Background()
-//	ctx := context.WithValue(c, constant.OperationID, operationID)
-//	return SetOperationID(ctx, operationID)
-//}
-//
-//func SetOperationID(ctx context.Context, operationID string) context.Context {
-//	return context.WithValue(ctx, constant.OperationID, operationID)
-//}
-//
-//func SetOpUserID(ctx context.Context, opUserID string) context.Context {
-//	return context.WithValue(ctx, constant.OpUserID, opUserID)
-//}
-//
-//func SetConnID(ctx context.Context, connID string) context.Context {
-//	return context.WithValue(ctx, constant.ConnID, connID)
-//}
-//
-//func GetOperationID(ctx context.Context) string {
-//	if ctx.Value(constant.OperationID) != nil {
-//		s, ok := ctx.Value(constant.OperationID).(string)
-//		if ok {
-//			return s
-//		}
-//	}
-//	return ""
-//}
-//func GetOpUserID(ctx context.Context) string {
-//	if ctx.Value(constant.OpUserID) != "" {
-//		s, ok := ctx.Value(constant.OpUserID).(string)
-//		if ok {
-//			return s
-//		}
-//	}
-//	return ""
-//}
-//func GetConnID(ctx context.Context) string {
-//	if ctx.Value(constant.ConnID) != "" {
-//		s, ok := ctx.Value(constant.ConnID).(string)
-//		if ok {
-//			return s
-//		}
-//	}
-//	return ""
-//}
-//
-//func GetTriggerID(ctx context.Context) string {
-//	if ctx.Value(constant.TriggerID) != "" {
-//		s, ok := ctx.Value(constant.TriggerID).(string)
-//		if ok {
-//			return s
-//		}
-//	}
-//	return ""
-//}
-//func GetPlatform(ctx context.Context) int32 {
-//	if ctx.Value(Platform) != "" {
-//		s, ok := ctx.Value(Platform).(int32)
-//		if ok {
-//			return s
-//		}
-//	}
-//	return 0
-//}
-//func GetOpUserPlatform(ctx context.Context) string {
-//	if ctx.Value(constant.OpUserPlatform) != "" {
-//		s, ok := ctx.Value(constant.OpUserPlatform).(string)
-//		if ok {
-//			return s
-//		}
-//	}
-//	return ""
-//}
-//func GetDataDir(ctx context.Context) string {
-//	if ctx.Value(DataDir) != "" {
-//		s, ok := ctx.Value(DataDir).(string)
-//		if ok {
-//			return s
-//		}
-//	}
-//	return ""
-//}
-//func GetRemoteAddr(ctx context.Context) string {
-//	if ctx.Value(constant.RemoteAddr) != "" {
-//		s, ok := ctx.Value(constant.RemoteAddr).(string)
-//		if ok {
-//			return s
-//		}
-//	}
-//	return ""
-//}
-//
-//func GetMustCtxInfo(ctx context.Context) (operationID, opUserID, platform, connID string, err error) {
-//	operationID, ok := ctx.Value(constant.OperationID).(string)
-//	if !ok {
-//		err = errs.ErrArgs.Wrap("ctx missing operationID")
-//		return
-//	}
-//	opUserID, ok1 := ctx.Value(constant.OpUserID).(string)
-//	if !ok1 {
-//		err = errs.ErrArgs.Wrap("ctx missing opUserID")
-//		return
-//	}
-//	platform, ok2 := ctx.Value(constant.OpUserPlatform).(string)
-//	if !ok2 {
-//		err = errs.ErrArgs.Wrap("ctx missing platform")
-//		return
-//	}
-//	connID, _ = ctx.Value(constant.ConnID).(string)
-//	return
-//
-//}
-//func WithMustInfoCtx(values []interface{}) (context.Context, context.CancelFunc) {
-//	ctx, cancel := context.WithCancel(context.Background())
-//	for i, v := range values {
-//		ctx = context.WithValue(ctx, mapper[i], v)
-//
-//	}
-//	return ctx, cancel
-//
-//}
