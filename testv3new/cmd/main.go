7350d85d657ddc57e9b7144e5949b9a1667ec8d9
wangchuxiao <wangchuxiao97@outlook.com>
2023-07-21 20:37:00 +0800

    test: pressure test

--- testv3new/cmd/main.go
+++ testv3new/cmd/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"open_im_sdk/pkg/utils"
+	"open_im_sdk/testv3new"
+	"open_im_sdk/testv3new/testcore"
+
+	"github.com/OpenIMSDK/Open-IM-Server/pkg/common/log"
+	"github.com/OpenIMSDK/Open-IM-Server/pkg/common/mcontext"
+)
+
+func main() {
+	if err := log.InitFromConfig("sdk.log", "sdk", 3, true, false, "", 2); err != nil {
+		panic(err)
+	}
+	userID := "4844258055"
+	recvID := "4950983283"
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiI0ODQ0MjU4MDU1IiwiUGxhdGZvcm1JRCI6MiwiZXhwIjoxNjk3NzE4Mzk4LCJuYmYiOjE2ODk5NDIwOTgsImlhdCI6MTY4OTk0MjM5OH0.5d2O6yMFtyqdkkOLosYtxQoOtfsMSHn85HdQOzSX3Ok"
+	ctx := testv3new.NewCtx(testcore.APIADDR, testcore.WSADDR, userID, token)
+	baseCore := testcore.NewBaseCore(ctx, userID)
+	ctx = mcontext.SetOperationID(ctx, utils.OperationIDGenerator())
+	if err := baseCore.SendSingleMsg(ctx, recvID, 0); err != nil {
+		panic(err)
+	}
+}
--- testv3new/pressure_tester.go
+++ testv3new/pressure_tester.go
@@ -6,7 +6,6 @@ import (
 	"open_im_sdk/pkg/constant"
 	"open_im_sdk/sdk_struct"
 	"open_im_sdk/testv3new/testcore"
-	"sync"
 	"time"
 
 	"github.com/OpenIMSDK/Open-IM-Server/pkg/common/log"
@@ -25,15 +24,17 @@ func NewPressureTester(apiAddr, wsAddr string) *PressureTester {
 	return &PressureTester{sendLightWeightSDKCores: map[string]*testcore.BaseCore{}, recvLightWeightSDKCores: map[string]*testcore.BaseCore{}}
 }
 
-func (p *PressureTester) newCtx(userID, token string) context.Context {
-	return ccontext.WithInfo(context.Background(), &ccontext.GlobalConfig{UserID: userID, Token: token, IMConfig: sdk_struct.IMConfig{
-		PlatformID:          constant.AndroidPlatformID,
-		ApiAddr:             p.apiAddr,
-		WsAddr:              p.wsAddr,
-		LogLevel:            2,
-		IsLogStandardOutput: true,
-		LogFilePath:         "./",
-	}})
+func NewCtx(apiAddr, wsAddr, userID, token string) context.Context {
+	return ccontext.WithInfo(context.Background(), &ccontext.GlobalConfig{
+		UserID: userID, Token: token,
+		IMConfig: sdk_struct.IMConfig{
+			PlatformID:          constant.AndroidPlatformID,
+			ApiAddr:             apiAddr,
+			WsAddr:              wsAddr,
+			LogLevel:            2,
+			IsLogStandardOutput: true,
+			LogFilePath:         "./",
+		}})
 }
 
 func (p *PressureTester) initCores(m *map[string]*testcore.BaseCore, userIDs []string) {
@@ -44,7 +45,7 @@ func (p *PressureTester) initCores(m *map[string]*testcore.BaseCore, userIDs []s
 			continue
 		}
 		mV := *m
-		mV[userID] = testcore.NewBaseCore(p.newCtx(userID, token), userID)
+		mV[userID] = testcore.NewBaseCore(NewCtx(p.apiAddr, p.wsAddr, userID, token), userID)
 	}
 }
 
@@ -74,19 +75,11 @@ func (p *PressureTester) OrderingSendMsg(groupID string, msgNum int) {
 // reliability test
 func (p *PressureTester) MsgReliabilityTest(sendUserID, recvUserID string, msgNum int, duration time.Duration) {
 	sendCore := p.sendLightWeightSDKCores[sendUserID]
-	// recvCore := p.recvLightWeightSDKCores[recvUserID]
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		for i := 0; i < msgNum; i++ {
-			ctx := context.Background()
-			if err := sendCore.SendSingleMsg(ctx, recvUserID, i); err != nil {
-				log.ZError(ctx, "send msg error", err, "index", i, "recvUserID", recvUserID, "sendUserID", sendUserID)
-			}
-		}
-		wg.Done()
-	}()
-	wg.Wait()
+	ctx := context.Background()
+
+	if err := sendCore.SendSingleMsg(ctx, recvUserID, 1); err != nil {
+		log.ZError(ctx, "send msg error", err, "index", 1, "recvUserID", recvUserID, "sendUserID", sendUserID)
+	}
 
 	// log.ZInfo(context.Background(), "send msg done", "reliability", recvCore.GetRecvMsgNum() == msgNum)
 }
