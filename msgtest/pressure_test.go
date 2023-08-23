31ce0c8a28091d66cc243e9d2808c7b86467a4ce
wangchuxiao <wangchuxiao97@outlook.com>
2023-08-23 17:52:07 +0800

    feat: pressure send msg

--- msgtest/pressure_test.go
+++ msgtest/pressure_test.go
@@ -3,6 +3,7 @@ package msgtest
 import (
 	"context"
 	"flag"
+	"fmt"
 	"open_im_sdk/msgtest/module"
 	"open_im_sdk/sdk_struct"
 	"sync"
@@ -11,17 +12,6 @@ import (
 
 	"github.com/OpenIMSDK/tools/log"
 )
-
-func InitWithFlag() {
-	flag.IntVar(&totalOnlineUserNum, "t", 100000, "total online user num")
-	flag.IntVar(&friendMsgSenderNum, "f", 100, "friend msg sender num")
-	flag.IntVar(&NotFriendMsgSenderNum, "n", 100, "not friend msg sender num")
-	flag.IntVar(&groupMsgSenderNum, "g", 100, "group msg sender num")
-	flag.IntVar(&msgSenderNumEvreyUser, "m", 100, "msg sender num evrey user")
-
-	flag.IntVar(&recvMsgUserNum, "r", 20, "recv msg user num")
-
-}
 
 const (
 	TenThousandGroupUserNum = 10000
@@ -33,6 +23,9 @@ const (
 	ThousandGroupNum    = 5
 	HundredGroupNum     = 50
 	FiftyGroupNum       = 100
+
+	FastenedUserPrefix = "fastened_user_prefix"
+	RecvMsgPrefix      = "recv_msg_prefix"
 )
 
 var (
@@ -41,9 +34,22 @@ var (
 	NotFriendMsgSenderNum int // 非好友消息发送者数
 	groupMsgSenderNum     int // 群消息发送者数
 	msgSenderNumEvreyUser int // 每个用户的消息数
+	fastenedUserNum       int // 固定用户数
 
 	recvMsgUserNum int // 消息接收者数, 抽样账号
+
 )
+
+func InitWithFlag() {
+	flag.IntVar(&totalOnlineUserNum, "t", 100000, "total online user num")
+	flag.IntVar(&friendMsgSenderNum, "f", 100, "friend msg sender num")
+	flag.IntVar(&NotFriendMsgSenderNum, "n", 100, "not friend msg sender num")
+	flag.IntVar(&groupMsgSenderNum, "g", 100, "group msg sender num")
+	flag.IntVar(&msgSenderNumEvreyUser, "m", 100, "msg sender num evrey user")
+
+	flag.IntVar(&recvMsgUserNum, "r", 20, "recv msg user num")
+	flag.IntVar(&fastenedUserNum, "u", 320, "fastened user num")
+}
 
 func init() {
 	InitWithFlag()
@@ -54,60 +60,35 @@ func init() {
 	}
 }
 
-func Test_Pressure(t *testing.T) {
+func Test_PressureFull(t *testing.T) {
 	if friendMsgSenderNum+NotFriendMsgSenderNum+groupMsgSenderNum > totalOnlineUserNum {
 		t.Fatal("sender num > total online user num")
 	}
+
 	p := NewPressureTester()
-	// sample recv msg user
-	recvMsgUserIDs := p.userManager.GenUserIDs(recvMsgUserNum)
-	userIDs := p.userManager.GenUserIDs(totalOnlineUserNum)
-	var groupSenderUserIDs, friendSenderUserIDs, notfriendSenderUserIDs []string
-	if err := p.userManager.RegisterUsers(userIDs...); err != nil {
-		t.Fatal(err)
+	// gen userIDs
+	userIDs, fastenedUserIDs, recvMsgUserIDs := p.genUserIDs()
+
+	// register
+	if err := p.registerUsers(userIDs, fastenedUserIDs, recvMsgUserIDs); err != nil {
+		t.Fatalf("register users failed, err: %v", err)
 	}
-	for i, userID := range userIDs {
-		token, err := p.userManager.GetToken(userID, int32(PLATFORMID))
-		if err != nil {
-			log.ZError(context.Background(), "get token failed", err)
-			continue
-		}
-		user := module.NewUser(userID, token, sdk_struct.IMConfig{WsAddr: WSADDR, ApiAddr: APIADDR, PlatformID: int32(PLATFORMID)})
-		if 0 <= i && i < friendMsgSenderNum {
-			p.msgSender[userID] = user
-			friendSenderUserIDs = append(friendSenderUserIDs, userID)
-		} else if friendMsgSenderNum <= i && i < friendMsgSenderNum+NotFriendMsgSenderNum {
-			p.msgSender[userID] = user
-			notfriendSenderUserIDs = append(notfriendSenderUserIDs, userID)
-		} else if friendMsgSenderNum+NotFriendMsgSenderNum <= i && i < friendMsgSenderNum+NotFriendMsgSenderNum+groupMsgSenderNum {
-			p.groupMsgSender[userID] = user
-			groupSenderUserIDs = append(groupSenderUserIDs, userID)
-		}
-	}
-	tenThousandGroupIDs, thousandGroupIDs, hundredGroupUserIDs, fiftyGroupUserIDs, err := p.createTestGroups(userIDs)
+	// init users
+	p.initUserConns(userIDs, fastenedUserIDs)
+
+	// create groups
+	err := p.createTestGroups(userIDs, fastenedUserIDs, recvMsgUserIDs)
 	if err != nil {
 		t.Fatal(err)
 	}
-	totalGroupIDs := append(append(append(tenThousandGroupIDs, thousandGroupIDs...), hundredGroupUserIDs...), fiftyGroupUserIDs...)
+
 	// import friends
-	for _, recvMsgUserID := range recvMsgUserIDs {
-		p.friendManager.ImportFriends(recvMsgUserID, friendSenderUserIDs)
+	if err := p.importFriends(p.friendSenderUserIDs, fastenedUserIDs); err != nil {
+		t.Fatal(err)
 	}
-	var wg sync.WaitGroup
-	wg.Add(3)
-	go func() {
-		defer wg.Done()
-		p.sendMsgs2Users(friendSenderUserIDs, recvMsgUserIDs, msgSenderNumEvreyUser, time.Second)
-	}()
-	go func() {
-		defer wg.Done()
-		p.sendMsgs2Users(notfriendSenderUserIDs, recvMsgUserIDs, msgSenderNumEvreyUser, time.Second)
-	}()
-	go func() {
-		defer wg.Done()
-		p.sendMsgs2Groups(groupSenderUserIDs, totalGroupIDs, msgSenderNumEvreyUser, time.Second)
-	}()
-	wg.Wait()
+
+	p.pressureSendMsg()
+	// send msg test
 }
 
 type PressureTester struct {
@@ -116,6 +97,11 @@ type PressureTester struct {
 	groupManager   *module.TestGroupManager
 	msgSender      map[string]*module.SendMsgUser
 	groupMsgSender map[string]*module.SendMsgUser
+
+	groupSenderUserIDs, friendSenderUserIDs, notfriendSenderUserIDs []string
+	recvMsgUserIDs                                                  []string
+
+	tenThousandGroupIDs, thousandGroupIDs, hundredGroupUserIDs, fiftyGroupUserIDs []string
 }
 
 func NewPressureTester() *PressureTester {
@@ -124,8 +110,88 @@ func NewPressureTester() *PressureTester {
 		msgSender: make(map[string]*module.SendMsgUser), groupMsgSender: make(map[string]*module.SendMsgUser)}
 }
 
-func (p *PressureTester) createTestGroups(userIDs []string) (tenThousandGroupIDs, thousandGroupIDs, hundredGroupUserIDs, fiftyGroupUserIDs []string, err error) {
-	p.groupManager.CreateGroup("", "", userIDs[0], userIDs[:TenThousandGroupUserNum])
+func (p *PressureTester) genUserIDs() (userIDs, fastenedUserIDs, recvMsgUserIDs []string) {
+	userIDs = p.userManager.GenUserIDs(totalOnlineUserNum - fastenedUserNum)                  // 在线用户
+	fastenedUserIDs = p.userManager.GenUserIDsWithPrefix(fastenedUserNum, FastenedUserPrefix) // 指定320用户
+	recvMsgUserIDs = p.userManager.GenUserIDsWithPrefix(recvMsgUserNum, RecvMsgPrefix)        // 抽样用户完整SDK
+	return
+}
+
+func (p *PressureTester) registerUsers(userIDs []string, fastenedUserIDs []string, recvMsgUserIDs []string) error {
+	if err := p.userManager.RegisterUsers(userIDs...); err != nil {
+		return err
+	}
+	if err := p.userManager.RegisterUsers(fastenedUserIDs...); err != nil {
+		return err
+	}
+	if err := p.userManager.RegisterUsers(recvMsgUserIDs...); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *PressureTester) initUserConns(userIDs []string, fastenedUserIDs []string) {
+	userIDs = append(userIDs, fastenedUserIDs...)
+	for i, userID := range userIDs {
+		token, err := p.userManager.GetToken(userID, int32(PLATFORMID))
+		if err != nil {
+			log.ZError(context.Background(), "get token failed", err, "userID", userID, "platformID", PLATFORMID)
+			continue
+		}
+		user := module.NewUser(userID, token, sdk_struct.IMConfig{WsAddr: WSADDR, ApiAddr: APIADDR, PlatformID: int32(PLATFORMID)})
+		if 0 <= i && i < friendMsgSenderNum {
+			p.msgSender[userID] = user
+			p.friendSenderUserIDs = append(p.friendSenderUserIDs, userID)
+		} else if friendMsgSenderNum <= i && i < friendMsgSenderNum+NotFriendMsgSenderNum {
+			p.msgSender[userID] = user
+			p.notfriendSenderUserIDs = append(p.notfriendSenderUserIDs, userID)
+		} else if friendMsgSenderNum+NotFriendMsgSenderNum <= i && i < friendMsgSenderNum+NotFriendMsgSenderNum+groupMsgSenderNum {
+			p.groupMsgSender[userID] = user
+			p.groupSenderUserIDs = append(p.groupSenderUserIDs, userID)
+		}
+	}
+}
+
+func (p *PressureTester) createTestGroups(userIDs, fastenedUserIDs, recvMsgUserIDs []string) (err error) {
+	// create ten thousand group
+	for i := 1; i <= TenThousandGroupNum; i++ {
+		groupID := p.groupManager.GenGroupID(fmt.Sprintf("tenThousandGroup_%d", i))
+		err = p.groupManager.CreateGroup(groupID, "tenThousandGroup", userIDs[0], append(userIDs[(i-1)*TenThousandGroupUserNum:i*TenThousandGroupUserNum-1], fastenedUserIDs...))
+		if err != nil {
+			return
+		}
+		p.tenThousandGroupIDs = append(p.tenThousandGroupIDs, groupID)
+	}
+	// create thousand group
+	exclude := TenThousandGroupNum * TenThousandGroupUserNum
+	for i := 1; i <= ThousandGroupNum; i++ {
+		groupID := p.groupManager.GenGroupID(fmt.Sprintf("thousandGroup_%d", i))
+		err = p.groupManager.CreateGroup(groupID, "thousandGroup", userIDs[0], append(userIDs[exclude+(i-1)*ThousandGroupUserNum:exclude+i*ThousandGroupUserNum-1], fastenedUserIDs...))
+		if err != nil {
+			return
+		}
+		p.thousandGroupIDs = append(p.thousandGroupIDs, groupID)
+	}
+	// create hundred group
+	exclude += exclude + ThousandGroupNum*ThousandGroupUserNum
+	for i := 1; i <= HundredGroupNum; i++ {
+		groupID := p.groupManager.GenGroupID(fmt.Sprintf("hundredGroup_%d", i))
+		err = p.groupManager.CreateGroup(groupID, "hundredGroup", userIDs[0], append(fastenedUserIDs[0:80], recvMsgUserIDs...))
+		if err != nil {
+			return
+		}
+		p.hundredGroupUserIDs = append(p.hundredGroupUserIDs, groupID)
+	}
+	// create fifty group
+	exclude += exclude + HundredGroupNum*HundredGroupUserNum
+	for i := 1; i <= FiftyGroupNum; i++ {
+		groupID := p.groupManager.GenGroupID(fmt.Sprintf("fiftyGroup_%d", i))
+		err = p.groupManager.CreateGroup(groupID, "fiftyGroup", userIDs[0], append(fastenedUserIDs[0:30], recvMsgUserIDs...))
+		if err != nil {
+			return
+		}
+		p.fiftyGroupUserIDs = append(p.fiftyGroupUserIDs, groupID)
+	}
 	return
 }
 
@@ -154,6 +220,7 @@ func (p *PressureTester) sendMsgs2Groups(senderIDs, groupIDs []string, num int, 
 		for _, groupID := range groupIDs {
 			wg.Add(1)
 			go func(senderID, groupID string) {
+				defer wg.Done()
 				for i := 0; i < num; i++ {
 					if user, ok := p.groupMsgSender[senderID]; ok {
 						user.SendGroupMsgWithContext(groupID, i)
@@ -164,4 +231,32 @@ func (p *PressureTester) sendMsgs2Groups(senderIDs, groupIDs []string, num int, 
 		}
 	}
 	wg.Wait()
+}
+
+func (p *PressureTester) pressureSendMsg() {
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		p.sendMsgs2Users(p.friendSenderUserIDs, p.recvMsgUserIDs, msgSenderNumEvreyUser, time.Second)
+	}()
+	go func() {
+		defer wg.Done()
+		p.sendMsgs2Users(p.notfriendSenderUserIDs, p.recvMsgUserIDs, msgSenderNumEvreyUser, time.Second)
+	}()
+	go func() {
+		defer wg.Done()
+		totalGroupIDs := append(append(p.tenThousandGroupIDs, p.thousandGroupIDs...), append(p.hundredGroupUserIDs, p.fiftyGroupUserIDs...)...)
+		p.sendMsgs2Groups(p.groupSenderUserIDs, totalGroupIDs, msgSenderNumEvreyUser, time.Second)
+	}()
+	wg.Wait()
+}
+
+func (p *PressureTester) importFriends(friendSenderUserIDs, recvMsgUserIDs []string) error {
+	for _, recvMsgUserID := range recvMsgUserIDs {
+		if err := p.friendManager.ImportFriends(recvMsgUserID, friendSenderUserIDs); err != nil {
+			return err
+		}
+	}
+	return nil
 }
