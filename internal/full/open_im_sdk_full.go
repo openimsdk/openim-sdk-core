cf2480304ae30c2f5305dc7e51ac70edf03a6433
Gordon <46924906+FGadvancer@users.noreply.github.com>
2023-08-29 19:55:03 +0800

    fix: do not update group message when get user info.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- internal/full/open_im_sdk_full.go
+++ internal/full/open_im_sdk_full.go
@@ -16,11 +16,11 @@ package full
 
 import (
 	"context"
+	"github.com/OpenIMSDK/tools/log"
+	"open_im_sdk/pkg/common"
+	"open_im_sdk/pkg/constant"
 	"open_im_sdk/pkg/db/model_struct"
 	api "open_im_sdk/pkg/server_api_params"
-	"open_im_sdk/pkg/utils"
-
-	"github.com/OpenIMSDK/tools/log"
 )
 
 func (u *Full) GetUsersInfo(ctx context.Context, userIDs []string) ([]*api.FullUserInfo, error) {
@@ -69,55 +69,18 @@ func (u *Full) GetUsersInfo(ctx context.Context, userIDs []string) ([]*api.FullU
 		// update single conversation
 
 		conversation, err := u.db.GetConversationByUserID(ctx, userID)
-		if err != nil || conversation.ConversationID == "" {
-			log.ZWarn(ctx, "GetUsersInfo GetConversationByUserID failed", err, "userID", userID)
+		if err != nil {
+			log.ZWarn(ctx, "GetConversationByUserID failed", err, "userID", userID)
 		} else {
+			if _, ok := friendMap[userID]; ok {
+				continue
+			}
 			log.ZDebug(ctx, "GetConversationByUserID", "conversation", conversation)
 			if conversation.ShowName != info.Nickname || conversation.FaceURL != info.FaceURL {
-				friends, err := u.db.GetFriendInfoList(ctx, []string{userID})
-				if err != nil {
-					log.ZError(ctx, "GetUsersInfo GetFriendInfoList failed", err, "userID", userID)
-				} else {
-					// not friend
-					if len(friends) == 0 {
-						conversation.ShowName = info.Nickname
-					} else {
-						// friend and have remark
-						if friends[0].Remark != "" {
-							conversation.ShowName = friends[0].Remark
-						}
-					}
-				}
-				conversation.FaceURL = info.FaceURL
-				if err := u.db.UpdateConversation(ctx, conversation); err != nil {
-					log.ZError(ctx, "GetUsersInfo UpdateMsgSenderFaceURLAndSenderNickname failed", err, "userID", userID)
-				} else {
-					u.conversationListner.OnConversationChanged(utils.StructToJsonString([]*model_struct.LocalConversation{conversation}))
-				}
-			}
-		}
-		// update joined groups
-		groupIDs, err := u.db.GetUserJoinedGroupIDs(ctx, userID)
-		if err != nil {
-			log.ZError(ctx, "GetUsersInfo GetUserJoinedGroupIDs failed", err, "userID", userID)
-		} else {
-			for _, groupID := range groupIDs {
-				localGroupMember, err := u.db.GetGroupMemberInfoByGroupIDUserID(ctx, groupID, userID)
-				if err != nil {
-					log.ZError(ctx, "GetUsersInfo GetGroupMemberInfoByGroupIDUserID failed", err, "userID", userID, "groupID", groupID)
-				} else {
-					if localGroupMember.Nickname != info.Nickname || localGroupMember.FaceURL != info.FaceURL {
-						if err := u.db.UpdateMsgSenderFaceURLAndSenderNickname(ctx, utils.GetConversationIDByGroupID(groupID), userID, info.FaceURL, info.Nickname); err != nil {
-							log.ZError(ctx, "UpdateMsgSenderFaceURLAndSenderNickname failed", err, "conversationID", utils.GetConversationIDByGroupID(groupID), "userID", userID, "nickname", info.Nickname, "faceURL", info.FaceURL)
-						}
-						localGroupMember.FaceURL = info.FaceURL
-						localGroupMember.Nickname = info.Nickname
-						if err := u.db.UpdateGroupMember(ctx, localGroupMember); err != nil {
-							log.ZError(ctx, "UpdateGroupMember failed", err, "userID", userID)
-						}
-					}
-				}
-
+				_ = common.TriggerCmdUpdateConversation(ctx, common.UpdateConNode{Action: constant.UpdateConFaceUrlAndNickName,
+					Args: common.SourceIDAndSessionType{SourceID: userID, SessionType: constant.SingleChatType, FaceURL: info.FaceURL, Nickname: info.Nickname}}, u.ch)
+				_ = common.TriggerCmdUpdateMessage(ctx, common.UpdateMessageNode{Action: constant.UpdateMsgFaceUrlAndNickName,
+					Args: common.UpdateMessageInfo{UserID: userID, FaceURL: info.FaceURL, Nickname: info.Nickname}}, u.ch)
 			}
 		}
 	}
