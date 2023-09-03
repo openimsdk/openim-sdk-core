4471f5f777abfadfdf99b3a22e65ab7ab9a3575a
plutoyty <2631223275@qq.com>
2023-09-03 11:24:06 +0800

    feat:Caching of personal information of strangers in group chats

--- pkg/db/stranger_model.go
+++ pkg/db/stranger_model.go
@@ -1,3 +1,20 @@
+// Copyright © 2023 OpenIM SDK. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build !js
+// +build !js
+
 package db
 
 import (
@@ -6,7 +23,7 @@ import (
 	"open_im_sdk/pkg/utils"
 )
 
-func (d *DataBase) GetStrangerInfo(ctx context.Context, userIDs []string) ([]*model_struct.LocalStranger, error) {
+func (d DataBase) GetStrangerInfo(ctx context.Context, userIDs []string) ([]*model_struct.LocalStranger, error) {
 	d.friendMtx.Lock()
 	defer d.friendMtx.Unlock()
 	var friendList []model_struct.LocalStranger
