24a1a58aad44ba0a5ad1ab47e6eaac3e7ee21e89
chao <48119764+withchao@users.noreply.github.com>
2023-11-01 20:58:15 -0500

    feat: file read cache (#396)
    * chore: default thumbnail size
    
    * fix: upload logs error invalid memory address or nil pointer dereference
    
    * fix: upload logs error invalid memory address or nil pointer dereference
    
    * fix: when uploading logs, other ctrip writes caused upload failure
    
    * fix: file msg progress
    
    * fix: file msg progress
    
    * test: file msg progress
    
    * fix: file msg progress
    
    * feat: file read cache

--- testv2/file_test.go
+++ testv2/file_test.go
@@ -14,67 +14,25 @@
 
 package testv2
 
-//import (
-//	"flag"
-//	"fmt"
-//	"github.com/OpenIMSDK/tools/mcontext"
-//	"open_im_sdk/internal/file"
-//	"open_im_sdk/open_im_sdk"
-//	"path/filepath"
-//	"testing"
-//	"time"
-//)
-//
-//type FilePutCallback struct{}
-//
-//func (c *FilePutCallback) Open(size int64) {
-//	fmt.Println("open put file", size)
-//}
-//
-//func (c *FilePutCallback) HashProgress(current, total int64) {
-//	//fmt.Println("hash", current, total)
-//}
-//
-//func (c *FilePutCallback) HashComplete(hash string, total int64) {
-//	fmt.Println("hash complete", hash, total)
-//}
-//
-//func (c *FilePutCallback) PutStart(current, total int64) {
-//	fmt.Println("put start", current, total)
-//}
-//
-//func (c *FilePutCallback) PutProgress(save int64, current, total int64) {
-//	fmt.Printf("put progress [%d/%d] put %f%% save %f%%\n", current, total, float64(current)/float64(total)*100, float64(save)/float64(total)*100)
-//}
-//
-//func (c *FilePutCallback) PutComplete(total int64, putType int) {
-//	fmt.Println("put complete", total, putType)
-//}
-//
-//func TestPut(t *testing.T) {
-//	ctx := mcontext.NewCtx("123456")
-//
-//	putID := "asdkmjakslaaa"
-//
-//	go func() {
-//		time.Sleep(time.Second * 3)
-//		fmt.Println("###################### CANCEL PUT ######################")
-//		open_im_sdk.UserForSDK.File().Cancel(ctx, putID)
-//	}()
-//
-//	req := &file.PutArgs{
-//		PutID:    putID,
-//		Filepath: "C:\\Users\\Admin\\Desktop\\VMware-workstation-full-17.0.0-20800274.exe",
-//	}
-//	req.Name = filepath.Base(req.Filepath)
-//	str, err := open_im_sdk.UserForSDK.File().PutFile(ctx, req, &FilePutCallback{})
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	fmt.Println("url", str)
-//}
-//
-//func Test_Fmt(t *testing.T) {
-//	i := flag.Int("sn", 2, "sender num")
-//	fmt.Println(i)
-//}
+import (
+	"github.com/openimsdk/openim-sdk-core/v3/internal/file"
+	"github.com/openimsdk/openim-sdk-core/v3/open_im_sdk"
+	"path/filepath"
+	"testing"
+)
+
+func TestUploadFile(t *testing.T) {
+
+	fp := `C:\Users\openIM\Desktop\dist.zip`
+
+	resp, err := open_im_sdk.UserForSDK.File().UploadFile(ctx, &file.UploadFileReq{
+		Filepath: fp,
+		Name:     filepath.Base(fp),
+		Cause:    "test",
+	}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(resp)
+
+}
