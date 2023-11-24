3058eef32d1cf709b0bb69de3f79c325ac430477
Gordon <46924906+FGadvancer@users.noreply.github.com>
2023-11-24 19:38:39 +0800

    fix: single message test.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- msgtest/main/main.go
+++ msgtest/main/main.go
@@ -33,7 +33,7 @@ func main() {
 	p.InitUserConns(f)
 	log.ZDebug(ctx, "all user init connect to server success,start send message")
 	time.Sleep(10 * time.Second)
-	p.SendSingleMessages(f, 100, time.Second)
+	p.SendSingleMessages(f, 10, time.Second)
 	log.ZDebug(ctx, "message send finished start to check message")
 	time.Sleep(20 * time.Second)
 	p.CheckMsg()
