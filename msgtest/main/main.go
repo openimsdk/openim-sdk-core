848791e8dccac0f3dff0e72152506737ad10b4d6
Gordon <46924906+FGadvancer@users.noreply.github.com>
2023-11-28 16:00:25 +0800

    fix: single message test.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- msgtest/main/main.go
+++ msgtest/main/main.go
@@ -51,17 +51,17 @@ func main() {
 	log.ZDebug(ctx, "Sample UserID", "sampleUserLength", len(r), "sampleUserID", r, "length", len(f))
 	time.Sleep(10 * time.Second)
 	//
-	if err := p.RegisterUsers(f, nil, nil); err != nil {
-		log.ZError(ctx, "Sample UserID failed", err)
-		return
-	}
+	//if err := p.RegisterUsers(f, nil, nil); err != nil {
+	//	log.ZError(ctx, "Sample UserID failed", err)
+	//	return
+	//}
 	// init users
 	p.InitUserConns(f)
-	log.ZDebug(ctx, "all user init connect to server success,start send message")
-	time.Sleep(10 * time.Second)
-	p.SendSingleMessages(f, 10, time.Millisecond*100)
-	time.Sleep(5 * time.Minute)
-	p.CheckMsg(ctx)
+	//log.ZDebug(ctx, "all user init connect to server success,start send message")
+	//time.Sleep(10 * time.Second)
+	//p.SendSingleMessages(f, 10, time.Millisecond*100)
+	//time.Sleep(5 * time.Minute)
+	//p.CheckMsg(ctx)
 
 	time.Sleep(time.Hour * 60)
 
