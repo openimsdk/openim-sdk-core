c8d640c70e08eaf061a59fac7a89271096cdc968
Gordon <46924906+FGadvancer@users.noreply.github.com>
2023-12-04 22:04:59 +0800

    fix: single message test.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- msgtest/module/pressure.go
+++ msgtest/module/pressure.go
@@ -242,16 +242,17 @@ func (p *PressureTester) SendSingleMessages(fastenedUserIDs []string, num int, d
 				receiverUserIDs = append(receiverUserIDs, fastenedUserIDs[index])
 			}
 		}
-		for j, rv := range receiverUserIDs {
-			if user, ok := p.msgSender[userID]; ok {
-				wg.Add(1)
-				go func() {
-					defer wg.Done()
+		wg.Add(1)
+		go func(receiverUserIDs []string) {
+			defer wg.Done()
+			for j, rv := range receiverUserIDs {
+				if user, ok := p.msgSender[userID]; ok {
 					user.SendMsgWithContext(rv, j)
-				}()
+				}
+				time.Sleep(duration)
+
 			}
-			time.Sleep(duration)
-		}
+		}(receiverUserIDs)
 	}
 	wg.Wait()
 
