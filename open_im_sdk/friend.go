2b3c31e0e099522fda31cddcdf352c199f8b7f7c
AndrewZuo01 <andrew.zuo@wustl.edu>
2023-12-12 17:25:38 +0800

    update name JSON to friendIDsAndIsPinned

--- open_im_sdk/friend.go
+++ open_im_sdk/friend.go
@@ -43,8 +43,8 @@ func AddFriend(callback open_im_sdk_callback.Base, operationID string, userIDReq
 func SetFriendRemark(callback open_im_sdk_callback.Base, operationID string, userIDRemark string) {
 	call(callback, operationID, UserForSDK.Friend().SetFriendRemark, userIDRemark)
 }
-func SetFriendPin(callback open_im_sdk_callback.Base, operationID string, JSON string) {
-	call(callback, operationID, UserForSDK.Friend().SetFriendsPin, JSON)
+func SetFriendPin(callback open_im_sdk_callback.Base, operationID string, friendIDsAndIsPinned string) {
+	call(callback, operationID, UserForSDK.Friend().SetFriendsPin, friendIDsAndIsPinned)
 }
 func DeleteFriend(callback open_im_sdk_callback.Base, operationID string, friendUserID string) {
 	call(callback, operationID, UserForSDK.Friend().DeleteFriend, friendUserID)
