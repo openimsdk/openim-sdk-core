3f832ceed4c3d40ff46888b124f9eb1e437b46a4
Gordon <1432970085@qq.com>
2023-12-19 01:06:11 +0800

    chore: fix bool.

--- msgtest/main/main.go
+++ msgtest/main/main.go
@@ -56,8 +56,8 @@ func InitWithFlag() {
 	flag.IntVar(&groupMsgSenderNum, "g", 100, "group msg sender num")
 	flag.IntVar(&msgSenderNumEvreyUser, "m", 100, "msg sender num evrey user")
 
-	flag.BoolVar(&isRegisterUser, "r", false, "register user to IM system")
-    flag.BoolVar(&onlineUsersOnly, "u", false, "consider only online users")
+	isRegisterUser = *flag.Bool( "r", false, "register user to IM system")
+    onlineUsersOnly = *flag.Bool( "u", false, "consider only online users")
 
 }
 
