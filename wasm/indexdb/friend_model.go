19b97c4766391f402810b1492b084da048c3058c
AndrewZuo01 <andrew.zuo@wustl.edu>
2023-12-25 14:22:54 +0800

    update data transfer bug

--- wasm/indexdb/friend_model.go
+++ wasm/indexdb/friend_model.go
@@ -107,8 +107,7 @@ func (i *Friend) SearchFriendList(ctx context.Context, keyword string, isSearchU
 		}
 	}
 }
-func (i *Friend) UpdateColumnsFriend(ctx context.Context, friendIDs string, args map[string]interface{}) error {
-
+func (i *Friend) UpdateColumnsFriend(ctx context.Context, friendIDs []string, args map[string]interface{}) error {
 	_, err := exec.Exec(utils.StructToJsonString(friendIDs), utils.StructToJsonString(args))
 	if err != nil {
 		return err // Return immediately if there's an error with any friendID
