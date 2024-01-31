09afaa2998e38260b57a95a8f8cd7d9e24a7b82d
Gordon <46924906+FGadvancer@users.noreply.github.com>
2024-01-31 18:07:56 +0800

    test: ip change.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- msgtest/module/pressure.go
+++ msgtest/module/pressure.go
@@ -16,7 +16,7 @@ import (
 )
 
 var (
-	TESTIP        = "172.19.26.147"
+	TESTIP        = "172.19.26.146"
 	APIADDR       = fmt.Sprintf("http://%v:20002", TESTIP)
 	WSADDR        = fmt.Sprintf("ws://%v:20001", TESTIP)
 	SECRET        = "openIM123"
