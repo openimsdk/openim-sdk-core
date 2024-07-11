adcb016e2fa19853ca473e4598cb3ddc10b24651
Monet Lee <monet_lee@163.com>
2024-07-11 14:58:06 +0800

    update method args.

--- open_im_sdk/userRelated.go
+++ open_im_sdk/userRelated.go
@@ -459,7 +459,7 @@ func (u *LoginMgr) setAppBackgroundStatus(ctx context.Context, isBackground bool
 		u.longConnMgr.SetBackground(isBackground)
 		if isBackground == false {
 			_ = common.TriggerCmdWakeUp(u.heartbeatCmdCh)
-			_ = common.TriggerCmdActiveSyncConversation(u.heartbeatCmdCh)
+			_ = common.TriggerCmdActiveSyncConversation(u.conversationCh)
 		}
 		return nil
 	}
