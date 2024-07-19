b9d6a9c6697d9e62d2aac896a5d1901bd2f4b271
icey-yu <1186114839@qq.com>
2024-07-19 11:59:59 +0800

    feat: log test

--- testv2/third_test.go
+++ testv2/third_test.go
@@ -15,7 +15,7 @@ func (s SProgress) OnProgress(current int64, size int64) {
 
 func Test_UploadLog(t *testing.T) {
 	tm := time.Now()
-	err := open_im_sdk.UserForSDK.Third().UploadLogs(ctx, "测试type", "这是一个ex", SProgress{})
+	err := open_im_sdk.UserForSDK.Third().UploadLogs(ctx, "it is ex", SProgress{})
 	if err != nil {
 		t.Error(err)
 	}
