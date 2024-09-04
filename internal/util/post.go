e38ff12439ee0345f100c6a617fbdb0caa8b946b
icey-yu <1186114839@qq.com>
2024-09-04 17:42:39 +0800

    fix: change http timeout

--- internal/util/post.go
+++ internal/util/post.go
@@ -34,7 +34,7 @@ import (
 
 // apiClient is a global HTTP client with a timeout of one minute.
 var apiClient = &http.Client{
-	Timeout: time.Second * 30,
+	Timeout: time.Second * 300,
 }
 
 // ApiResponse represents the standard structure of an API response.
