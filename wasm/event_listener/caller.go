87734dc9ed4bcdf044644bccd7cb0c779ffd503c
Monet Lee <monet_lee@163.com>
2024-09-13 16:50:03 +0800

    remove unused import pkg.

--- wasm/event_listener/caller.go
+++ wasm/event_listener/caller.go
@@ -21,15 +21,15 @@ import (
 	"bytes"
 	"context"
 	"errors"
-	"github.com/openimsdk/tools/errs"
 	"reflect"
 	"strconv"
 	"strings"
 	"syscall/js"
 
+	"github.com/openimsdk/tools/errs"
+
 	"github.com/openimsdk/openim-sdk-core/v3/pkg/utils"
 	"github.com/openimsdk/openim-sdk-core/v3/wasm/exec"
-	"github.com/openimsdk/tools/errs"
 	"github.com/openimsdk/tools/log"
 )
 
