35af02a4a00c0437ca5bf8e1c31d220f515e43d7
Monet Lee <monet_lee@163.com>
2024-10-14 16:56:53 +0800

    feat: update fn call error stack.

--- open_im_sdk/caller.go
+++ open_im_sdk/caller.go
@@ -18,11 +18,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"math"
 	"reflect"
 	"runtime"
 	"runtime/debug"
-	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -30,12 +28,9 @@ import (
 	"github.com/openimsdk/openim-sdk-core/v3/open_im_sdk_callback"
 	"github.com/openimsdk/openim-sdk-core/v3/pkg/ccontext"
 	"github.com/openimsdk/openim-sdk-core/v3/pkg/sdkerrs"
-	"github.com/openimsdk/protocol/errinfo"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 
 	"github.com/openimsdk/tools/log"
-	"github.com/openimsdk/tools/mw/specialerror"
+	"github.com/openimsdk/tools/mw"
 
 	"github.com/openimsdk/tools/errs"
 )
@@ -115,7 +110,7 @@ func call_(operationID string, fn any, args ...any) (res any, err error) {
 			if err == nil {
 				log.ZInfo(ctx, "fn call success", "function name", funcName, "cost time", fmt.Sprintf("%d ms", elapsed), "resp", res)
 			} else {
-				log.ZError(ctx, "fn call error", err, "function name", funcName, "cost time", fmt.Sprintf("%d ms", elapsed))
+				log.ZError(ctx, "fn call error", mw.FormatError(err), "function name", funcName, "cost time", fmt.Sprintf("%d ms", elapsed))
 
 			}
 
@@ -187,7 +182,7 @@ func call_(operationID string, fn any, args ...any) (res any, err error) {
 
 	if fnt.Out(len(outs) - 1).Implements(reflect.ValueOf(new(error)).Elem().Type()) {
 		if errValueOf := outs[len(outs)-1]; !errValueOf.IsNil() {
-			return nil, handleCallError(ctx, funcName, args, errValueOf.Interface().(error))
+			return nil, errValueOf.Interface().(error)
 		}
 		if len(outs) == 1 {
 			return "", nil
@@ -501,75 +496,4 @@ func listenerCall(fn any, listener any) {
 	}
 	args := reflect.ValueOf(listener)
 	fnv.Call([]reflect.Value{args})
-}
-
-func handleCallError(ctx context.Context, funcName string, args []any, err error) error {
-	log.ZWarn(ctx, "fn call WithDetails Response is error", formatError(err), "funcName", funcName, "args", args)
-	unwrap := errs.Unwrap(err)
-	codeErr := specialerror.ErrCode(unwrap)
-	if codeErr == nil {
-		log.ZError(ctx, "internal server error", formatError(err), "funcName", funcName, "args", args)
-		codeErr = errs.ErrInternalServer
-	}
-	code := codeErr.Code()
-	if code <= 0 || int64(code) > int64(math.MaxUint32) {
-		log.ZError(ctx, "unknown error code", formatError(err), "funcName", funcName, "args", args, "unknown code:", int64(code))
-		code = errs.ServerInternalError
-	}
-	grpcStatus := status.New(codes.Code(code), err.Error())
-	errInfo := &errinfo.ErrorInfo{Cause: err.Error()}
-	details, err := grpcStatus.WithDetails(errInfo)
-	if err != nil {
-		log.ZWarn(ctx, "fn call WithDetails Response is error", formatError(err), "funcName", funcName)
-		return errs.WrapMsg(err, "fn error in setting grpc status details", "err", err)
-	}
-	log.ZWarn(ctx, "fn call Response is error", details.Err())
-
-	return nil
-}
-
-func formatError(err error) error {
-	type stackTracer interface {
-		StackTrace() errors.StackTrace
-	}
-	if e, ok := err.(stackTracer); ok {
-		st := e.StackTrace()
-		var sb strings.Builder
-		sb.WriteString("Error: ")
-		sb.WriteString(err.Error())
-		sb.WriteString(" | Error trace: ")
-
-		var callPath []string
-		for _, f := range st {
-			pc := uintptr(f) - 1
-			fn := runtime.FuncForPC(pc)
-			if fn == nil {
-				continue
-			}
-			if strings.Contains(fn.Name(), "runtime.") {
-				continue
-			}
-			file, line := fn.FileLine(pc)
-			funcName := simplifyFuncName(fn.Name())
-			callPath = append(callPath, fmt.Sprintf("%s (%s:%d)", funcName, file, line))
-		}
-		for i := len(callPath) - 1; i >= 0; i-- {
-			if i != len(callPath)-1 {
-				sb.WriteString(" -> ")
-			}
-			sb.WriteString(callPath[i])
-		}
-		return errors.New(sb.String())
-	}
-	return err
-}
-
-func simplifyFuncName(fullFuncName string) string {
-	parts := strings.Split(fullFuncName, "/")
-	lastPart := parts[len(parts)-1]
-	parts = strings.Split(lastPart, ".")
-	if len(parts) > 1 {
-		return parts[len(parts)-1]
-	}
-	return lastPart
 }
