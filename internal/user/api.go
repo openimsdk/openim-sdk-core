e2389f146306ba5c1d924da78f89463c43917a25
Monet Lee <monet_lee@163.com>
2024-10-25 17:31:53 +0800

    fix: improve batchUserFaceURLandName logic. (#756)
    * refactor: update ServerAPI method name.
    
    * feat: improve batchUserFaceURLandName logic.
    
    * update logic.

--- internal/user/api.go
+++ internal/user/api.go
@@ -16,9 +16,9 @@ import (
 	"github.com/openimsdk/tools/utils/datautil"
 )
 
-// GetSingleUserFromSvr retrieves user information from the server.
-func (u *User) GetSingleUserFromSvr(ctx context.Context, userID string) (*model_struct.LocalUser, error) {
-	users, err := u.GetUsersInfoFromSvr(ctx, []string{userID})
+// GetSingleUserFromServer retrieves user information from the server.
+func (u *User) GetSingleUserFromServer(ctx context.Context, userID string) (*model_struct.LocalUser, error) {
+	users, err := u.GetUsersInfoFromServer(ctx, []string{userID})
 	if err != nil {
 		return nil, err
 	}
@@ -125,10 +125,18 @@ func (u *User) ProcessUserCommandUpdate(ctx context.Context, userCommand *userPb
 
 // GetUserInfoFromServer retrieves user information from the server.
 func (u *User) GetUserInfoFromServer(ctx context.Context, userIDs []string) ([]*model_struct.LocalUser, error) {
+	var err error
+
 	serverUsersInfo, err := u.getUsersInfo(ctx, userIDs)
 	if err != nil {
 		return nil, err
 	}
+
+	if len(serverUsersInfo) == 0 {
+		log.ZError(ctx, "serverUsersInfo is empty", err, "userIDs", userIDs)
+		return nil, err
+	}
+
 	return datautil.Batch(ServerUserToLocalUser, serverUsersInfo), nil
 }
 
@@ -171,11 +179,11 @@ func (u *User) GetUsersInfo(ctx context.Context, userIDs []string) ([]*sdk_struc
 	return res, nil
 }
 
-// GetUsersInfoFromSvr retrieves user information from the server.
-func (u *User) GetUsersInfoFromSvr(ctx context.Context, userIDs []string) ([]*model_struct.LocalUser, error) {
+// GetUsersInfoFromServer retrieves user information from the server.
+func (u *User) GetUsersInfoFromServer(ctx context.Context, userIDs []string) ([]*model_struct.LocalUser, error) {
 	users, err := u.getUsersInfo(ctx, userIDs)
 	if err != nil {
-		return nil, sdkerrs.WrapMsg(err, "GetUsersInfoFromSvr failed")
+		return nil, sdkerrs.WrapMsg(err, "GetUsersInfoFromServer failed")
 	}
 	return datautil.Batch(ServerUserToLocalUser, users), nil
 }
