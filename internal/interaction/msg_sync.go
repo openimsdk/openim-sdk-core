e0dea5d6ac3302a2487c24f03a0c73df3c89f92e
icey-yu <119291641+icey-yu@users.noreply.github.com>
2024-11-22 15:33:22 +0800

    Fix：Change check reinstall logic (#789)
    * fix: err
    
    * fix: err
    
    * fix: err
    
    * fix: err
    
    * fix: err

--- internal/interaction/msg_sync.go
+++ internal/interaction/msg_sync.go
@@ -16,7 +16,9 @@ package interaction
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"gorm.io/gorm"
 	"runtime/debug"
 	"strings"
 	"sync"
@@ -91,10 +93,10 @@ func (m *MsgSyncer) loadSeq(ctx context.Context) error {
 
 	if len(conversationIDList) == 0 {
 		version, err := m.db.GetAppSDKVersion(ctx)
-		if err != nil {
+		if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 			return err
 		}
-		if !version.Installed {
+		if version == nil || !version.Installed {
 			m.reinstalled = true
 		}
 	}
@@ -107,16 +109,19 @@ func (m *MsgSyncer) loadSeq(ctx context.Context) error {
 		Err            error
 	}
 
-	concurrency := 20
-	partSize := len(conversationIDList) / concurrency
+	partSize := 20
+	currency := (len(conversationIDList)-1)/partSize + 1
+	if len(conversationIDList) == 0 {
+		currency = 0
+	}
 	var wg sync.WaitGroup
-	resultMaps := make([]map[string]SyncedSeq, concurrency)
+	resultMaps := make([]map[string]SyncedSeq, currency)
 
-	for i := 0; i < concurrency; i++ {
+	for i := 0; i < currency; i++ {
 		wg.Add(1)
 		start := i * partSize
 		end := start + partSize
-		if i == concurrency-1 {
+		if i == currency-1 {
 			end = len(conversationIDList)
 		}
 
