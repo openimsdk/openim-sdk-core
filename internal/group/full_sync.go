527015aa4cdbe56d66f69b694266ac7f43608623
Gordon <46924906+FGadvancer@users.noreply.github.com>
2025-04-28 17:10:48 +0800

    fix: failure to clear @ tags in conversations, group member synchronization errors, and friend list synchronization anomalies,due to concurrency.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- internal/group/full_sync.go
+++ internal/group/full_sync.go
@@ -21,14 +21,6 @@ import (
 	"github.com/openimsdk/tools/utils/datautil"
 )
 
-func (g *Group) SyncAllJoinedGroupsAndMembers(ctx context.Context) error {
-	if err := g.IncrSyncJoinGroup(ctx); err != nil {
-		return err
-	}
-	//return g.IncrSyncJoinGroupMember(ctx)
-	return nil
-}
-
 func (g *Group) SyncAllSelfGroupApplication(ctx context.Context) error {
 	if !g.groupRequestSyncerLock.TryLock() {
 		return nil
