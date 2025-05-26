57440f770298858390453d7b91473b3a535bc641
Gordon <46924906+FGadvancer@users.noreply.github.com>
2025-05-26 16:55:15 +0800

    fix: refactor friend request and group request handling by removing local caching to relieve synchronization performance pressure.
    Signed-off-by: Gordon <46924906+FGadvancer@users.noreply.github.com>

--- open_im_sdk/group.go
+++ open_im_sdk/group.go
@@ -120,12 +120,12 @@ func RefuseGroupApplication(callback open_im_sdk_callback.Base, operationID stri
 	call(callback, operationID, IMUserContext.Group().RefuseGroupApplication, groupID, fromUserID, handleMsg)
 }
 
-func CheckLocalGroupFullSync(callback open_im_sdk_callback.Base, operationID string, groupID string, fromUserID string, handleMsg string) {
-	call(callback, operationID, IMUserContext.Group().CheckLocalGroupFullSync, groupID, fromUserID, handleMsg)
+func CheckLocalGroupFullSync(callback open_im_sdk_callback.Base, operationID string) {
+	call(callback, operationID, IMUserContext.Group().CheckLocalGroupFullSync)
 }
 
-func CheckGroupMemberFullSync(callback open_im_sdk_callback.Base, operationID string, groupID string, fromUserID string, handleMsg string) {
-	call(callback, operationID, IMUserContext.Group().CheckGroupMemberFullSync, groupID, fromUserID, handleMsg)
+func CheckGroupMemberFullSync(callback open_im_sdk_callback.Base, operationID string, groupID string) {
+	call(callback, operationID, IMUserContext.Group().CheckGroupMemberFullSync, groupID)
 }
 
 func GetGroupApplicationUnhandledCount(callback open_im_sdk_callback.Base, operationID string) {
